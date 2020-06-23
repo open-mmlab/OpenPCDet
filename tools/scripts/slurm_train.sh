acc9dd2639c20a99bfb673e6d3b2528d9b2b0dbe
Shaoshuai Shi <shaoshuaics@gmail.com>
2020-06-23 20:47:27 +0800

    add training and testing scripts

---
+++ tools/scripts/slurm_train.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -x
+
+PARTITION=$1
+JOB_NAME=$2
+GPUS=$3
+PY_ARGS=${@:4}
+
+GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+CPUS_PER_TASK=${CPUS_PER_TASK:-5}
+SRUN_ARGS=${SRUN_ARGS:-""}
+
+
+srun -p ${PARTITION} \
+    --job-name=${JOB_NAME} \
+    --gres=gpu:${GPUS_PER_NODE} \
+    --ntasks=${GPUS} \
+    --ntasks-per-node=${GPUS_PER_NODE} \
+    --cpus-per-task=${CPUS_PER_TASK} \
+    --kill-on-bad-exit=1 \
+    ${SRUN_ARGS} \
+    python -u train.py --launcher slurm ${PY_ARGS}
