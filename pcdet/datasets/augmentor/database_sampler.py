5904f8e3fbe825af704a145178d7585e4e3a9d31
Shaoshuai Shi <shaoshuaics@gmail.com>
2020-07-03 14:08:33 +0800

    remove warning from yaml and dbsampler (#121)
    * remove warning from yaml and dbsampler

--- pcdet/datasets/augmentor/database_sampler.py
+++ pcdet/datasets/augmentor/database_sampler.py
@@ -164,7 +164,7 @@ class DataBaseSampler(object):
 
         """
         gt_boxes = data_dict['gt_boxes']
-        gt_names = data_dict['gt_names']
+        gt_names = data_dict['gt_names'].astype(str)
         existed_boxes = gt_boxes
         total_valid_sampled_dict = []
         for class_name, sample_group in self.sample_groups.items():
