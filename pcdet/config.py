0807d89d46024a045d21092aee8bdeddfe09ee71
Shaoshuai Shi <shaoshuaics@gmail.com>
2020-07-03 14:05:49 +0800

    remove warning from yaml and dbsampler

--- pcdet/config.py
+++ pcdet/config.py
@@ -51,9 +51,9 @@ def merge_new_config(config, new_config):
     if '_BASE_CONFIG_' in new_config:
         with open(new_config['_BASE_CONFIG_'], 'r') as f:
             try:
-                yaml_config = yaml.load(f)
-            except:
                 yaml_config = yaml.load(f, Loader=yaml.FullLoader)
+            except:
+                yaml_config = yaml.load(f)
         config.update(EasyDict(yaml_config))
 
     for key, val in new_config.items():
@@ -70,9 +70,9 @@ def merge_new_config(config, new_config):
 def cfg_from_yaml_file(cfg_file, config):
     with open(cfg_file, 'r') as f:
         try:
-            new_config = yaml.load(f)
-        except:
             new_config = yaml.load(f, Loader=yaml.FullLoader)
+        except:
+            new_config = yaml.load(f)
 
         merge_new_config(config=config, new_config=new_config)
 
