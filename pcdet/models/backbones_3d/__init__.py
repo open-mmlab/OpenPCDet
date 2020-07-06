7d25122061e16140d97a9b485933eaf4c60e9111
Shaoshuai Shi <shaoshuaics@gmail.com>
2020-07-06 16:28:44 +0800

    add VoxelResBackbone8x 3D backbone

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -1,7 +1,8 @@
-from .spconv_backbone import VoxelBackBone8x
+from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x
 from .spconv_unet import UNetV2
 
 __all__ = {
     'VoxelBackBone8x': VoxelBackBone8x,
+    'VoxelResBackBone8x': VoxelResBackBone8x,
     'UNetV2': UNetV2
 }
