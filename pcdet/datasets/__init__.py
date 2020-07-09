0dd5326e84d03a28c8742519375f6a145037f0a8
Shaoshuai Shi <shaoshuaics@gmail.com>
2020-07-09 19:07:01 +0800

    bugfixed: support merge_all_iters_to_one_epoch in dataloader

--- pcdet/datasets/__init__.py
+++ pcdet/datasets/__init__.py
@@ -37,7 +37,7 @@ class DistributedSampler(_DistributedSampler):
 
 
 def build_dataloader(dataset_cfg, class_names, batch_size, dist, root_path=None, workers=4,
-                     logger=None, training=True):
+                     logger=None, training=True, merge_all_iters_to_one_epoch=False, total_epochs=0):
 
     dataset = __all__[dataset_cfg.DATASET](
         dataset_cfg=dataset_cfg,
@@ -46,6 +46,11 @@ def build_dataloader(dataset_cfg, class_names, batch_size, dist, root_path=None,
         training=training,
         logger=logger,
     )
+
+    if merge_all_iters_to_one_epoch:
+        assert hasattr(dataset, 'merge_all_iters_to_one_epoch')
+        dataset.merge_all_iters_to_one_epoch(merge=True, epochs=total_epochs)
+
     if dist:
         if training:
             sampler = torch.utils.data.distributed.DistributedSampler(dataset)
