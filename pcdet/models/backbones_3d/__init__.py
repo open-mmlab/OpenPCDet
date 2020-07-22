0b21d8f92220daeee037d6a173b4cecd7f3913d4
Shaoshuai Shi <shaoshuaics@gmail.com>
2020-07-23 00:26:19 +0800

    support PointNet2Backbone

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -1,7 +1,9 @@
 from .spconv_backbone import VoxelBackBone8x
 from .spconv_unet import UNetV2
+from .pointnet2_backbone import PointNet2Backbone
 
 __all__ = {
     'VoxelBackBone8x': VoxelBackBone8x,
-    'UNetV2': UNetV2
+    'UNetV2': UNetV2,
+    'PointNet2Backbone': PointNet2Backbone
 }
