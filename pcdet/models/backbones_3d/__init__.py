308819a79c280bf7976f03c79e527e637dd9f362
MengAaron <871628893@qq.com>
2021-01-29 16:20:55 +0800

    add cars

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -1,6 +1,7 @@
 from .pointnet2_backbone import PointNet2Backbone, PointNet2MSG
 from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x
 from .spconv_unet import UNetV2
+from .rsn_backbone import CarS
 
 __all__ = {
     'VoxelBackBone8x': VoxelBackBone8x,
@@ -8,4 +9,5 @@ __all__ = {
     'PointNet2Backbone': PointNet2Backbone,
     'PointNet2MSG': PointNet2MSG,
     'VoxelResBackBone8x': VoxelResBackBone8x,
+    'CarS':CarS,
 }
