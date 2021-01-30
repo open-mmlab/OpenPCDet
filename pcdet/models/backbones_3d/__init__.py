9968c60eed958fcf4ae60e238e686ebb53a4b5da
MengAaron <871628893@qq.com>
2021-01-30 09:11:33 +0800

    add 3d backbone CarL

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -1,7 +1,7 @@
 from .pointnet2_backbone import PointNet2Backbone, PointNet2MSG
 from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x
 from .spconv_unet import UNetV2
-from .rsn_backbone import CarS
+from .rsn_backbone import CarS,CarL
 
 __all__ = {
     'VoxelBackBone8x': VoxelBackBone8x,
@@ -10,4 +10,5 @@ __all__ = {
     'PointNet2MSG': PointNet2MSG,
     'VoxelResBackBone8x': VoxelResBackBone8x,
     'CarS':CarS,
+    'CarL':CarL,
 }
