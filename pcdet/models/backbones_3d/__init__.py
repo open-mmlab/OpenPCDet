f011f0937a4a11ea6ef7614bd112a3a85ca143d0
MengAaron <871628893@qq.com>
2021-02-06 21:03:15 +0800

    add VoxelBackBone8x_4layer

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -1,5 +1,5 @@
 from .pointnet2_backbone import PointNet2Backbone, PointNet2MSG
-from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x
+from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x,VoxelBackBone8x_4layer
 from .spconv_unet import UNetV2
 from .rsn_backbone import CarS,CarL
 
@@ -9,6 +9,7 @@ __all__ = {
     'PointNet2Backbone': PointNet2Backbone,
     'PointNet2MSG': PointNet2MSG,
     'VoxelResBackBone8x': VoxelResBackBone8x,
-    'CarS':CarS,
-    'CarL':CarL,
+    'CarS': CarS,
+    'CarL': CarL,
+    'VoxelBackBone8x_4layer': VoxelBackBone8x_4layer,
 }
