54397699b09f8410e32885fddb16d10ec6d81ea4
MengAaron <871628893@qq.com>
2021-02-07 15:08:38 +0800

    make some change in proposal_layer and generate_boxes

--- pcdet/models/dense_heads/centerpoint_head.py
+++ pcdet/models/dense_heads/centerpoint_head.py
@@ -261,6 +261,7 @@ class CenterHead(nn.Module):
         super(CenterHead, self).__init__()
 
         self.model_cfg = model_cfg
+        self.post_cfg = model_cfg.TEST_CONFIG
         self.in_channels = input_channels
         self.num_class = num_class
         self.class_names = class_names
@@ -796,5 +797,197 @@ class CenterHead(nn.Module):
             predictions_dicts.append(predictions_dict)
         return predictions_dicts
 
+
     def generate_predicted_boxes(self,data_dict):
-        pass
+        """
+        Generate box predictions with decode, topk and circular nms
+        Args:
+            data_dict:
+
+        Returns:
+
+        """
+        double_flip = not self.training and self.post_cfg.get('double_flip', False) # type: bool
+        pre_dicts = self.forwad_ret_dict['multi_head_features'] # output of forward func.
+        post_center_range = self.post_cfg.post_center_limit_range
+
+        for task_id, pre_dict in enumerate(pre_dicts):
+            batch_size = pre_dict['hm'].shape[0]
+            if double_flip:
+                pass
+            else:
+                batch_hm = pre_dict['hm'].sigmoid()
+                batch_reg = pre_dict['reg']
+                batch_hei = pre_dict['height']
+                batch_dim = pre_dict['dim']
+                batch_rot_sine = pre_dict['rot'][:,0].unsqueeze(1)
+                batch_rot_cosine = pre_dict['rot'][:,1].unsqueeze(1)
+                bat_vel =pre_dict['vel']
+
+            # decode
+            boxes = self.proposal_layer(batch_hm,batch_rot_sine,batch_rot_cosine ,batch_hei,batch_dim,bat_vel,
+                                        reg=batch_reg,cfg=self.post_cfg,task_id=task_id)
+
+
+    def proposal_layer(self,heat,rot,hei,dim,vel,reg=None,
+                       post_center_range=None, score_threshold=None,cfg=None, raw_rot=False, task_d=-1):
+        """Decode bboxes.
+
+                Args:
+                    heat (torch.Tensor): Heatmap with the shape of [B, N, W, H].
+                    rot_sine (torch.Tensor): Sine of rotation with the shape of
+                        [B, 1, W, H].
+                    rot_cosine (torch.Tensor): Cosine of rotation with the shape of
+                        [B, 1, W, H].
+                    hei (torch.Tensor): Height of the boxes with the shape
+                        of [B, 1, W, H].
+                    dim (torch.Tensor): Dim of the boxes with the shape of
+                        [B, 1, W, H].
+                    vel (torch.Tensor): Velocity with the shape of [B, 1, W, H].
+                    reg (torch.Tensor): Regression value of the boxes in 2D with
+                        the shape of [B, 2, W, H]. Default: None.
+                    task_id (int): Index of task. Default: -1.
+
+                Returns:
+                    list[dict]: Decoded boxes.
+                """
+        batch, cat, _, _ = heat.size()
+
+        scores, inds, clses, ys, xs = self._topk(heat, K=self.max_num)
+
+        if reg is not None:
+            reg = self._transpose_and_gather_feat(reg, inds)
+            reg = reg.view(batch, self.max_num, 2)
+            xs = xs.view(batch, self.max_num, 1) + reg[:, :, 0:1]
+            ys = ys.view(batch, self.max_num, 1) + reg[:, :, 1:2]
+        else:
+            xs = xs.view(batch, self.max_num, 1) + 0.5
+            ys = ys.view(batch, self.max_num, 1) + 0.5
+
+        # rotation value and direction label
+        rot_sine = self._transpose_and_gather_feat(rot_sine, inds)
+        rot_sine = rot_sine.view(batch, self.max_num, 1)
+
+        rot_cosine = self._transpose_and_gather_feat(rot_cosine, inds)
+        rot_cosine = rot_cosine.view(batch, self.max_num, 1)
+        rot = torch.atan2(rot_sine, rot_cosine)
+
+        # height in the bev
+        hei = self._transpose_and_gather_feat(hei, inds)
+        hei = hei.view(batch, self.max_num, 1)
+
+        # dim of the box
+        dim = self._transpose_and_gather_feat(dim, inds)
+        dim = dim.view(batch, self.max_num, 3)
+
+        # class label
+        clses = clses.view(batch, self.max_num).float()
+        scores = scores.view(batch, self.max_num)
+
+        xs = xs.view(
+            batch, self.max_num,
+            1) * self.out_size_factor * self.voxel_size[0] + self.pc_range[0]
+        ys = ys.view(
+            batch, self.max_num,
+            1) * self.out_size_factor * self.voxel_size[1] + self.pc_range[1]
+
+        if vel is None:  # KITTI FORMAT
+            final_box_preds = torch.cat([xs, ys, hei, dim, rot], dim=2)
+        else:  # exist velocity, nuscene format
+            vel = self._transpose_and_gather_feat(vel, inds)
+            vel = vel.view(batch, self.max_num, 2)
+            final_box_preds = torch.cat([xs, ys, hei, dim, rot, vel], dim=2)
+
+        final_scores = scores
+        final_preds = clses
+
+        # use score threshold
+        if self.score_threshold is not None:
+            thresh_mask = final_scores > self.score_threshold
+
+        if self.post_center_range is not None:
+            self.post_center_range = torch.tensor(
+                self.post_center_range, device=heat.device)
+            mask = (final_box_preds[..., :3] >=
+                    self.post_center_range[:3]).all(2)
+            mask &= (final_box_preds[..., :3] <=
+                     self.post_center_range[3:]).all(2)
+
+            predictions_dicts = []
+            for i in range(batch):
+                cmask = mask[i, :]
+                if self.score_threshold:
+                    cmask &= thresh_mask[i]
+
+                boxes3d = final_box_preds[i, cmask]
+                scores = final_scores[i, cmask]
+                labels = final_preds[i, cmask]
+                predictions_dict = {
+                    'bboxes': boxes3d,
+                    'scores': scores,
+                    'labels': labels
+                }
+
+                predictions_dicts.append(predictions_dict)
+        else:
+            raise NotImplementedError(
+                'Need to reorganize output as a batch, only '
+                'support post_center_range is not None for now!')
+
+        return predictions_dicts
+
+
+    def _topk(self, scores, K=80):
+        """Get indexes based on scores.
+
+        Args:
+            scores (torch.Tensor): scores with the shape of [B, N, W, H].
+            K (int): Number to be kept. Defaults to 80.
+
+        Returns:
+            tuple[torch.Tensor]
+                torch.Tensor: Selected scores with the shape of [B, K].
+                torch.Tensor: Selected indexes with the shape of [B, K].
+                torch.Tensor: Selected classes with the shape of [B, K].
+                torch.Tensor: Selected y coord with the shape of [B, K].
+                torch.Tensor: Selected x coord with the shape of [B, K].
+        """
+        batch, cat, height, width = scores.size()
+
+        topk_scores, topk_inds = torch.topk(scores.view(batch, cat, -1), K)
+
+        topk_inds = topk_inds % (height * width)
+        topk_ys = (topk_inds.float() /
+                   torch.tensor(width, dtype=torch.float)).int().float()
+        topk_xs = (topk_inds % width).int().float()
+
+        topk_score, topk_ind = torch.topk(topk_scores.view(batch, -1), K)
+        topk_clses = (topk_ind / torch.tensor(K, dtype=torch.float)).int()
+        topk_inds = self._gather_feat(topk_inds.view(batch, -1, 1),
+                                      topk_ind).view(batch, K)
+        topk_ys = self._gather_feat(topk_ys.view(batch, -1, 1),
+                                    topk_ind).view(batch, K)
+        topk_xs = self._gather_feat(topk_xs.view(batch, -1, 1),
+                                    topk_ind).view(batch, K)
+
+        return topk_score, topk_inds, topk_clses, topk_ys, topk_xs
+
+    def _transpose_and_gather_feat(self, feat, ind):
+        """Given feats and indexes, returns the transposed and gathered feats.
+
+        Args:
+            feat (torch.Tensor): Features to be transposed and gathered
+                with the shape of [B, 2, W, H].
+            ind (torch.Tensor): Indexes with the shape of [B, N].
+
+        Returns:
+            torch.Tensor: Transposed and gathered feats.
+        """
+        feat = feat.permute(0, 2, 3, 1).contiguous()
+        feat = feat.view(feat.size(0), -1, feat.size(3))
+        feat = self._gather_feat(feat, ind)
+        return feat
+
+
+
+
