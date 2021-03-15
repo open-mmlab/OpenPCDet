b8cc391d2e10f7e947d53b0cd8ca7bc7b8850679
MengAaron <871628893@qq.com>
2021-03-15 16:50:24 +0800

    fix

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -1,7 +1,7 @@
 from .pointnet2_backbone import PointNet2Backbone, PointNet2MSG
-from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x,VoxelBackBone8x_4layer
+from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x, VoxelBackBone8x_4layer, SpMiddleResNetFHD
 from .spconv_unet import UNetV2
-from .rsn_backbone import CarS,CarL
+from .rsn_backbone import CarS, CarL
 
 __all__ = {
     'VoxelBackBone8x': VoxelBackBone8x,
@@ -12,4 +12,5 @@ __all__ = {
     'CarS': CarS,
     'CarL': CarL,
     'VoxelBackBone8x_4layer': VoxelBackBone8x_4layer,
+    'SpMiddleResNetFHD': SpMiddleResNetFHD
 }
