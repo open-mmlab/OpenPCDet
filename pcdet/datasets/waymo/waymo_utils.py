04b242272b5aacf0af64dce0762a56e197f2658b
MengAaron <871628893@qq.com>
2021-03-30 12:12:17 +0800

    fix

--- pcdet/datasets/waymo/waymo_utils.py
+++ pcdet/datasets/waymo/waymo_utils.py
@@ -293,10 +293,17 @@ def convert_point_to_cloud_range_image(data_dict):
     range_images = np.squeeze(range_images.numpy(), axis=0)
     data_dict['range_image'] = range_images
     gt_boxes = data_dict['gt_boxes']
-    box_idxs_of_pts = roiaware_pool3d_utils.points_in_boxes_gpu(
-        torch.from_numpy(points[..., :3]).float().cuda(),
-        torch.from_numpy(gt_boxes[:, 0:7]).unsqueeze(dim=0).float().cuda()
-    ).long().squeeze(dim=0).cpu().numpy()
+    # GPU method, but will meet CUDA subprocess error
+    # box_idxs_of_pts = roiaware_pool3d_utils.points_in_boxes_gpu(
+    #     torch.from_numpy(points[..., :3]).float().cuda(),
+    #     torch.from_numpy(gt_boxes[:, 0:7]).unsqueeze(dim=0).float().cuda()
+    # ).long().squeeze(dim=0).cpu().numpy()
+
+    # CPU method
+    box_idxs_of_pts = roiaware_pool3d_utils.points_in_boxes_cpu(
+        torch.from_numpy(points[..., :3]).float(),
+        torch.from_numpy(gt_boxes[:, 0:7]).unsqueeze(dim=0).float()
+    ).long().squeeze(dim=0)
     gt_points_vehicle_frame = tf.boolean_mask(points_vehicle_frame, box_idxs_of_pts > 0, axis=1)
     range_mask, ri_mask_indices, ri_mask_ranges = range_image_utils.build_range_image_from_point_cloud(
         gt_points_vehicle_frame, num_points, extrinsic, inclination, range_image_size)
