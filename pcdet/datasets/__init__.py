8be00d84fe64775fa81c72d795f6c16eefb4d472
MengAaron <871628893@qq.com>
2021-03-30 15:52:25 +0800

    fix

--- pcdet/datasets/__init__.py
+++ pcdet/datasets/__init__.py
@@ -50,7 +50,8 @@ def build_dataloader(dataset_cfg, class_names, batch_size, dist, root_path=None,
         training=training,
         logger=logger,
     )
-
+    for i in range(100):
+        data = dataset[i]
     if merge_all_iters_to_one_epoch:
         assert hasattr(dataset, 'merge_all_iters_to_one_epoch')
         dataset.merge_all_iters_to_one_epoch(merge=True, epochs=total_epochs)
