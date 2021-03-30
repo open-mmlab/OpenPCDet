32439d675c6fe73bc89a3056278ddf033ceedb87
MengAaron <871628893@qq.com>
2021-03-30 17:06:07 +0800

    fix

--- pcdet/datasets/waymo/waymo_utils.py
+++ pcdet/datasets/waymo/waymo_utils.py
@@ -13,8 +13,8 @@ from waymo_open_dataset.utils import frame_utils, transform_utils, range_image_u
 from waymo_open_dataset import dataset_pb2
 from ...ops.roiaware_pool3d import roiaware_pool3d_utils
 import torch
-import torch.nn as nn
-import pdb
+import numba
+import pudb
 
 try:
     tf.enable_eager_execution()
@@ -293,7 +293,7 @@ def convert_point_cloud_to_range_image(data_dict):
                                                                                                point_features)
     range_images = np.squeeze(range_images.numpy(), axis=0)
     data_dict['range_image'] = range_images
-    # gt_boxes = data_dict['gt_boxes']
+    gt_boxes = data_dict['gt_boxes']
 
     # GPU method, but will meet CUDA subprocess error
     # box_idxs_of_pts = roiaware_pool3d_utils.points_in_boxes_gpu(
@@ -307,14 +307,232 @@ def convert_point_cloud_to_range_image(data_dict):
     #     torch.from_numpy(points[..., :3].squeeze(axis=0)).float(),
     #     torch.from_numpy(gt_boxes[:, 0:7]).float()
     # ).long().numpy()
-    # flag_of_pts = point_indices.max(axis=0)
-    # select = flag_of_pts > 0
-    # data_dict['range_mask'] = select[0:10]
-    # gt_points_vehicle_frame = tf.boolean_mask(points_vehicle_frame, select, axis=1)
-    # range_mask, ri_mask_indices, ri_mask_ranges = range_image_utils.build_range_image_from_point_cloud(
-    #     gt_points_vehicle_frame, num_points, extrinsic, inclination, range_image_size)
-    # range_mask = np.squeeze(range_mask.numpy(), axis=0)
-    # range_mask[range_mask > 0] = 1
-    # data_dict['range_mask'] = range_mask
+    point_indices = points_in_rbbox(points[..., :3].squeeze(axis=0), gt_boxes)
+    flag_of_pts = point_indices.max(axis=0)
+    select = flag_of_pts > 0
+    gt_points_vehicle_frame = tf.boolean_mask(points_vehicle_frame, select, axis=1)
+    range_mask, ri_mask_indices, ri_mask_ranges = range_image_utils.build_range_image_from_point_cloud(
+        gt_points_vehicle_frame, num_points, extrinsic, inclination, range_image_size)
+    range_mask = np.squeeze(range_mask.numpy(), axis=0)
+    range_mask[range_mask > 0] = 1
+    data_dict['range_mask'] = range_mask
 
     return data_dict
+
+
+def points_in_rbbox(points, rbbox, z_axis=2, origin=(0.5, 0.5, 0.5)):
+    rbbox_corners = center_to_corner_box3d(
+        rbbox[:, :3], rbbox[:, 3:6], rbbox[:, -1], origin=origin, axis=z_axis
+    )
+    surfaces = corner_to_surfaces_3d(rbbox_corners)
+    indices = points_in_convex_polygon_3d_jit(points[:, :3], surfaces)
+    return indices
+
+
+def center_to_corner_box3d(centers, dims, angles=None, origin=(0.5, 0.5, 0.5), axis=2):
+    """convert kitti locations, dimensions and angles to corners
+
+    Args:
+        centers (float array, shape=[N, 3]): locations in kitti label file.
+        dims (float array, shape=[N, 3]): dimensions in kitti label file.
+        angles (float array, shape=[N]): rotation_y in kitti label file.
+        origin (list or array or float): origin point relate to smallest point.
+            use [0.5, 1.0, 0.5] in camera and [0.5, 0.5, 0] in lidar.
+        axis (int): rotation axis. 1 for camera and 2 for lidar.
+    Returns:
+        [type]: [description]
+    """
+    # 'length' in kitti format is in x axis.
+    # yzx(hwl)(kitti label file)<->xyz(lhw)(camera)<->z(-x)(-y)(wlh)(lidar)
+    # center in kitti format is [0.5, 1.0, 0.5] in xyz.
+    corners = corners_nd(dims, origin=origin)
+    # corners: [N, 8, 3]
+    if angles is not None:
+        corners = rotation_3d_in_axis(corners, angles, axis=axis)
+    corners += centers.reshape([-1, 1, 3])
+    return corners
+
+
+def corners_nd(dims, origin=0.5):
+    """generate relative box corners based on length per dim and
+    origin point.
+
+    Args:
+        dims (float array, shape=[N, ndim]): array of length per dim
+        origin (list or array or float): origin point relate to smallest point.
+
+    Returns:
+        float array, shape=[N, 2 ** ndim, ndim]: returned corners.
+        point layout example: (2d) x0y0, x0y1, x1y0, x1y1;
+            (3d) x0y0z0, x0y0z1, x0y1z0, x0y1z1, x1y0z0, x1y0z1, x1y1z0, x1y1z1
+            where x0 < x1, y0 < y1, z0 < z1
+    """
+    ndim = int(dims.shape[1])
+    corners_norm = np.stack(
+        np.unravel_index(np.arange(2 ** ndim), [2] * ndim), axis=1
+    ).astype(dims.dtype)
+    # now corners_norm has format: (2d) x0y0, x0y1, x1y0, x1y1
+    # (3d) x0y0z0, x0y0z1, x0y1z0, x0y1z1, x1y0z0, x1y0z1, x1y1z0, x1y1z1
+    # so need to convert to a format which is convenient to do other computing.
+    # for 2d boxes, format is clockwise start with minimum point
+    # for 3d boxes, please draw lines by your hand.
+    if ndim == 2:
+        # generate clockwise box corners
+        corners_norm = corners_norm[[0, 1, 3, 2]]
+    elif ndim == 3:
+        corners_norm = corners_norm[[0, 1, 3, 2, 4, 5, 7, 6]]
+    corners_norm = corners_norm - np.array(origin, dtype=dims.dtype)
+    corners = dims.reshape([-1, 1, ndim]) * corners_norm.reshape([1, 2 ** ndim, ndim])
+    return corners
+
+
+def corner_to_surfaces_3d(corners):
+    """convert 3d box corners from corner function above
+    to surfaces that normal vectors all direct to internal.
+
+    Args:
+        corners (float array, [N, 8, 3]): 3d box corners.
+    Returns:
+        surfaces (float array, [N, 6, 4, 3]):
+    """
+    # box_corners: [N, 8, 3], must from corner functions in this module
+    surfaces = np.array(
+        [
+            [corners[:, 0], corners[:, 1], corners[:, 2], corners[:, 3]],
+            [corners[:, 7], corners[:, 6], corners[:, 5], corners[:, 4]],
+            [corners[:, 0], corners[:, 3], corners[:, 7], corners[:, 4]],
+            [corners[:, 1], corners[:, 5], corners[:, 6], corners[:, 2]],
+            [corners[:, 0], corners[:, 4], corners[:, 5], corners[:, 1]],
+            [corners[:, 3], corners[:, 2], corners[:, 6], corners[:, 7]],
+        ]
+    ).transpose([2, 0, 1, 3])
+    return surfaces
+
+
+def rotation_3d_in_axis(points, angles, axis=0):
+    # points: [N, point_size, 3]
+    rot_sin = np.sin(angles)
+    rot_cos = np.cos(angles)
+    ones = np.ones_like(rot_cos)
+    zeros = np.zeros_like(rot_cos)
+    if axis == 1:
+        rot_mat_T = np.stack(
+            [
+                [rot_cos, zeros, -rot_sin],
+                [zeros, ones, zeros],
+                [rot_sin, zeros, rot_cos],
+            ]
+        )
+    elif axis == 2 or axis == -1:
+        rot_mat_T = np.stack(
+            [
+                [rot_cos, -rot_sin, zeros],
+                [rot_sin, rot_cos, zeros],
+                [zeros, zeros, ones],
+            ]
+        )
+    elif axis == 0:
+        rot_mat_T = np.stack(
+            [
+                [zeros, rot_cos, -rot_sin],
+                [zeros, rot_sin, rot_cos],
+                [ones, zeros, zeros],
+            ]
+        )
+    else:
+        raise ValueError("axis should in range")
+
+    return np.einsum("aij,jka->aik", points, rot_mat_T)
+
+
+def points_in_convex_polygon_3d_jit(points, polygon_surfaces, num_surfaces=None):
+    """check points is in 3d convex polygons.
+    Args:
+        points: [num_points, 3] array.
+        polygon_surfaces: [num_polygon, max_num_surfaces,
+            max_num_points_of_surface, 3]
+            array. all surfaces' normal vector must direct to internal.
+            max_num_points_of_surface must at least 3.
+        num_surfaces: [num_polygon] array. indicate how many surfaces
+            a polygon contain
+    Returns:
+        [num_points, num_polygon] bool array.
+    """
+    max_num_surfaces, max_num_points_of_surface = polygon_surfaces.shape[1:3]
+    num_points = points.shape[0]
+    num_polygons = polygon_surfaces.shape[0]
+    if num_surfaces is None:
+        num_surfaces = np.full((num_polygons,), 9999999, dtype=np.int64)
+    normal_vec, d = surface_equ_3d_jitv2(polygon_surfaces[:, :, :3, :])
+    # normal_vec: [num_polygon, max_num_surfaces, 3]
+    # d: [num_polygon, max_num_surfaces]
+    return _points_in_convex_polygon_3d_jit(
+        points, polygon_surfaces, normal_vec, d, num_surfaces
+    )
+
+
+@numba.njit
+def surface_equ_3d_jitv2(surfaces):
+    # polygon_surfaces: [num_polygon, num_surfaces, num_points_of_polygon, 3]
+    num_polygon = surfaces.shape[0]
+    max_num_surfaces = surfaces.shape[1]
+    normal_vec = np.zeros((num_polygon, max_num_surfaces, 3), dtype=surfaces.dtype)
+    d = np.zeros((num_polygon, max_num_surfaces), dtype=surfaces.dtype)
+    sv0 = surfaces[0, 0, 0] - surfaces[0, 0, 1]
+    sv1 = surfaces[0, 0, 0] - surfaces[0, 0, 1]
+    for i in range(num_polygon):
+        for j in range(max_num_surfaces):
+            sv0[0] = surfaces[i, j, 0, 0] - surfaces[i, j, 1, 0]
+            sv0[1] = surfaces[i, j, 0, 1] - surfaces[i, j, 1, 1]
+            sv0[2] = surfaces[i, j, 0, 2] - surfaces[i, j, 1, 2]
+            sv1[0] = surfaces[i, j, 1, 0] - surfaces[i, j, 2, 0]
+            sv1[1] = surfaces[i, j, 1, 1] - surfaces[i, j, 2, 1]
+            sv1[2] = surfaces[i, j, 1, 2] - surfaces[i, j, 2, 2]
+            normal_vec[i, j, 0] = sv0[1] * sv1[2] - sv0[2] * sv1[1]
+            normal_vec[i, j, 1] = sv0[2] * sv1[0] - sv0[0] * sv1[2]
+            normal_vec[i, j, 2] = sv0[0] * sv1[1] - sv0[1] * sv1[0]
+
+            d[i, j] = (
+                    -surfaces[i, j, 0, 0] * normal_vec[i, j, 0]
+                    - surfaces[i, j, 0, 1] * normal_vec[i, j, 1]
+                    - surfaces[i, j, 0, 2] * normal_vec[i, j, 2]
+            )
+    return normal_vec, d
+
+
+@numba.njit
+def _points_in_convex_polygon_3d_jit(
+        points, polygon_surfaces, normal_vec, d, num_surfaces=None
+):
+    """check points is in 3d convex polygons.
+    Args:
+        points: [num_points, 3] array.
+        polygon_surfaces: [num_polygon, max_num_surfaces,
+            max_num_points_of_surface, 3]
+            array. all surfaces' normal vector must direct to internal.
+            max_num_points_of_surface must at least 3.
+        num_surfaces: [num_polygon] array. indicate how many surfaces
+            a polygon contain
+    Returns:
+        [num_points, num_polygon] bool array.
+    """
+    max_num_surfaces, max_num_points_of_surface = polygon_surfaces.shape[1:3]
+    num_points = points.shape[0]
+    num_polygons = polygon_surfaces.shape[0]
+    ret = np.ones((num_points, num_polygons), dtype=np.bool_)
+    sign = 0.0
+    for i in range(num_points):
+        for j in range(num_polygons):
+            for k in range(max_num_surfaces):
+                if k > num_surfaces[j]:
+                    break
+                sign = (
+                        points[i, 0] * normal_vec[j, k, 0]
+                        + points[i, 1] * normal_vec[j, k, 1]
+                        + points[i, 2] * normal_vec[j, k, 2]
+                        + d[j, k]
+                )
+                if sign >= 0:
+                    ret[i, j] = False
+                    break
+    return ret
