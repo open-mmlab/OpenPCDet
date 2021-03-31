2a5025718f2c043028308cbe2d9751d4dee08c71
MengAaron <871628893@qq.com>
2021-03-31 16:33:56 +0800

    fix

--- pcdet/datasets/waymo/waymo_dataset.py
+++ pcdet/datasets/waymo/waymo_dataset.py
@@ -197,7 +197,8 @@ class WaymoDataset(DatasetTemplate):
                 'extrinsic': info['extrinsic'],
                 'range_image_shape': self.range_config.get('RANGE_IMAGE_SHAPE', [64, 2560])
             })
-            tf.convert_to_tensor(np.array([1,2,3])).numpy()
+            tf_tensor = tf.convert_to_tensor(np.array([1,2,3])).numpy()
+            torch_tensor = torch.tensor(tf_tensor)
             # data_dict = waymo_utils.convert_point_cloud_to_range_image(data_dict)
 
             data_dict.pop('points', None)
