f663b63121611310ca5ac629c052b654016c4dee
MengAaron <871628893@qq.com>
2021-04-07 11:22:18 +0800

    fix

--- pcdet/datasets/waymo/waymo_utils.py
+++ pcdet/datasets/waymo/waymo_utils.py
@@ -297,7 +297,6 @@ def convert_point_cloud_to_range_image(data_dict):
     data_dict['range_image'] = range_images
     data_dict['ri_indices'] = ri_indices
     gt_boxes = data_dict['gt_boxes']
-    pudb.set_trace()
     # CPU method, 0 or 1
     point_indices = roiaware_pool3d_utils.points_in_boxes_cpu(
         torch.from_numpy(points_vehicle_frame).float(),
