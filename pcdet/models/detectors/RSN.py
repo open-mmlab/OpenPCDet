d90420c33f2f56171f55b70cafce479fedd8d669
MengAaron <871628893@qq.com>
2021-04-11 17:34:28 +0800

    fix

--- pcdet/models/detectors/RSN.py
+++ pcdet/models/detectors/RSN.py
@@ -1,5 +1,7 @@
+import pudb
+
 from .detector3d_template import Detector3DTemplate
-import pdb
+import pudb
 
 
 class RSN(Detector3DTemplate):
@@ -8,7 +10,7 @@ class RSN(Detector3DTemplate):
         self.module_list = self.build_networks()
 
     def forward(self, batch_dict):
-
+        pudb.set_trace()
         for cur_module in self.module_list:
             batch_dict = cur_module(batch_dict)
 
