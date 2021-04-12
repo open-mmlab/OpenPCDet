c8ea6cdfc57b9e4b17ce26b7886ddf49d6373f07
MengAaron <871628893@qq.com>
2021-04-12 20:04:07 +0800

    fix

--- pcdet/models/seg_heads/map_to_point_cloud/point_gather.py
+++ pcdet/models/seg_heads/map_to_point_cloud/point_gather.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import pudb
 
 
 class PointGather(nn.Module):
@@ -11,24 +12,28 @@ class PointGather(nn.Module):
 
     def forward(self, batch_dict, **kwargs):
         # TODO: point features
-        range_features = batch_dict['range_features']
+        pudb.set_trace()
+        range_features = batch_dict['range_features'].permute((0, 2, 3, 1))
         seg_mask = batch_dict['seg_pred']
         batch_size, height, width = batch_dict['seg_pred'].shape
         points = batch_dict['points']
         ri_indices = batch_dict['ri_indices']
         foreground_points = []
         for batch_idx in range(batch_size):
+            this_range_features = range_features[batch_idx].reshape((height * width, -1))
             cur_seg_mask = seg_mask[batch_idx] >= self.foreground_threshold
             cur_seg_mask = torch.flatten(cur_seg_mask)
             batch_mask = points[:, 0] == batch_idx
             this_points = points[batch_mask, :]
             this_ri_indices = ri_indices[batch_mask, :]
             this_ri_indexes = this_ri_indices[:, 0] * width + this_ri_indices[:, 1]
-            this_points_mask = torch.gather(cur_seg_mask,dim=0,index=this_ri_indexes).bool()
+            this_points_mask = torch.gather(cur_seg_mask, dim=0, index=this_ri_indexes).bool()
             this_points = this_points[this_points_mask]
+            this_points_features = torch.gather(this_range_features, dim=0, index=this_ri_indexes)
+            this_points_features = this_points_features[this_points_mask]
+            this_points = torch.cat((this_points, this_points_features), dim=1)
             foreground_points.append(this_points)
 
-
-        foreground_points = torch.cat(foreground_points,dim=0)
+        foreground_points = torch.cat(foreground_points, dim=0)
         batch_dict['points'] = foreground_points
         return batch_dict
