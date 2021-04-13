e8ffb817cd5269377a281139794352244a045dbb
MengAaron <871628893@qq.com>
2021-04-13 10:28:10 +0800

    fix

--- pcdet/models/backbones_range/rsn_extractor.py
+++ pcdet/models/backbones_range/rsn_extractor.py
@@ -110,7 +110,6 @@ class UNet(nn.Module):
         self.out_channels = self.Up1.out_channels * 2
 
     def forward(self, batch_dict):
-        pudb.set_trace()
         x = batch_dict['range_image']
         conv1 = self.Down1(x)
         conv2 = self.Down2(conv1)
