5f7cea5ba565ac78effb9ebbcde7dcb7ffd46c40
MengAaron <871628893@qq.com>
2021-04-13 20:19:40 +0800

    fix

--- pcdet/models/seg_heads/map_to_point_cloud/point_gather.py
+++ pcdet/models/seg_heads/map_to_point_cloud/point_gather.py
@@ -47,6 +47,8 @@ class PointGather(nn.Module):
 
     def transform_points_to_voxels(self, data_dict=None, config=None):
         pudb.set_trace()
+        device = data_dict['points'].device
+        self.point_cloud_range = torch.tensor(self.point_cloud_range,device=device)
         try:
             from spconv.utils import VoxelGeneratorV2 as VoxelGenerator
         except:
@@ -58,7 +60,7 @@ class PointGather(nn.Module):
             max_num_points=config.MAX_POINTS_PER_VOXEL,
             max_voxels=config.MAX_NUMBER_OF_VOXELS[self.mode]
         )
-        grid_size = (self.point_cloud_range[3:6] - self.point_cloud_range[0:3]) / torch.tensor(config.VOXEL_SIZE)
+        grid_size = (self.point_cloud_range[3:6] - self.point_cloud_range[0:3]) / torch.tensor(config.VOXEL_SIZE,device=device)
         self.grid_size = torch.round(grid_size).int()
         self.voxel_size = config.VOXEL_SIZE
 
