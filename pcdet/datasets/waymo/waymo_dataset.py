d84f9ef81c9234453edb930e168166ca840ba077
MengAaron <871628893@qq.com>
2021-04-19 11:40:48 +0800

    fix

--- pcdet/datasets/waymo/waymo_dataset.py
+++ pcdet/datasets/waymo/waymo_dataset.py
@@ -197,8 +197,8 @@ class WaymoDataset(DatasetTemplate):
             # add key 'range_image'(C, H, W), 'range_mask'(H, W), 'ri_indices'(N, 2) to data_dict, 2 means H, W
             data_dict = waymo_utils.convert_point_cloud_to_range_image(data_dict)
             points_feature_num = data_dict['points'].shape[1]
-            data_dict['points'] = np.concatenate((data_dict['points'],data_dict['ri_indices']))
-            data_dict = self.prepare_data(data_dict=data_dict,augment=False)
+            data_dict['points'] = np.concatenate((data_dict['points'], data_dict['ri_indices']), axis=1)
+            data_dict = self.prepare_data(data_dict=data_dict, augment=False)
             data_dict['points'] = data_dict['points'][:, :points_feature_num]
             import pudb
             pudb.set_trace()
