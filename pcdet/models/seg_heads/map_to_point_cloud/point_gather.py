c4a026492799a0e909e5f4ace1e6fc6fddcfd35f
MengAaron <871628893@qq.com>
2021-04-21 18:32:19 +0800

    fix

--- pcdet/models/seg_heads/map_to_point_cloud/point_gather.py
+++ pcdet/models/seg_heads/map_to_point_cloud/point_gather.py
@@ -30,8 +30,8 @@ class PointGather(nn.Module):
         foreground_voxel_coords = []
         foreground_voxel_num_points = []
         import pudb
-        pudb.set_trace()
         import mayavi.mlab as mlab
+        pudb.set_trace()
 
         for batch_idx in range(batch_size):
             this_range_features = range_features[batch_idx].reshape((height * width, -1))
