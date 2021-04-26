ceb8877f1c6e280603b822cb6dceabd2007aaa39
MengAaron <871628893@qq.com>
2021-04-26 11:18:01 +0800

    fix

--- pcdet/models/backbones_3d/rsn_3d_backbone.py
+++ pcdet/models/backbones_3d/rsn_3d_backbone.py
@@ -228,7 +228,7 @@ class CarXL(nn.Module):
             conv3=B0Block(in_channels=out_channels, out_channels=out_channels, norm_fn=norm_fn, indice_key="subm_3"),
             conv4=B0Block(in_channels=out_channels, out_channels=out_channels, norm_fn=norm_fn, indice_key="subm_3"),
             conv5=B0Block(in_channels=out_channels, out_channels=out_channels, stride=2, norm_fn=norm_fn,
-                          indice_key="subm_5"),
+                          indice_key="subm_3"),
             conv6=B0Block(in_channels=out_channels, out_channels=out_channels, norm_fn=norm_fn, indice_key="subm_6"),
             conv7=B0Block(in_channels=out_channels, out_channels=out_channels, norm_fn=norm_fn, indice_key="subm_6"),
             conv8=B0Block(in_channels=out_channels, out_channels=out_channels, norm_fn=norm_fn, indice_key="subm_6")
