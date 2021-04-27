a2c44ab2092a073a5bce096bb81c4078731aaa1a
MengAaron <871628893@qq.com>
2021-04-27 10:10:21 +0800

    fix

--- pcdet/models/seg_heads/map_to_point_cloud/plot_utils.py
+++ pcdet/models/seg_heads/map_to_point_cloud/plot_utils.py
@@ -31,3 +31,21 @@ def map_plot_with_gt(batch_idx, batch_dict):
     this_points_mask = torch.gather(cur_seg_mask, dim=0, index=this_ri_indexes).bool()
     this_points = this_points[this_points_mask]
     plot_pc_with_gt(this_points, batch_idx, batch_dict)
+
+
+def plot_pc_with_gt_threshold(batch_idx, batch_dict, threshold=0.1):
+    seg_mask = batch_dict['seg_mask'] >= threshold
+    batch_size, height, width = seg_mask.shape
+    points = batch_dict['points']
+    ri_indices = batch_dict['ri_indices']
+    cur_seg_mask = seg_mask[batch_idx]
+    cur_seg_mask = torch.flatten(cur_seg_mask)
+
+    # points
+    batch_points_mask = points[:, 0] == batch_idx
+    this_points = points[batch_points_mask, :]
+    this_ri_indices = ri_indices[batch_points_mask, :]
+    this_ri_indexes = (this_ri_indices[:, 1] * width + this_ri_indices[:, 2]).long()
+    this_points_mask = torch.gather(cur_seg_mask, dim=0, index=this_ri_indexes).bool()
+    this_points = this_points[this_points_mask]
+    plot_pc_with_gt(this_points, batch_idx, batch_dict)
