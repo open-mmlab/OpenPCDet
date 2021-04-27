90047275fb618dc1899ccd9bc2c77f5f4be62ba5
MengAaron <871628893@qq.com>
2021-04-27 19:34:09 +0800

    fix

--- pcdet/models/seg_heads/map_to_point_cloud/plot_utils.py
+++ pcdet/models/seg_heads/map_to_point_cloud/plot_utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from ....datasets.waymo.waymo_utils import plot_pointcloud, plot_pointcloud_with_gt_boxes
 import torch
 
@@ -51,7 +53,7 @@ def plot_pc_with_gt_threshold(batch_dict, batch_idx=0, threshold=0.1):
     plot_pc_with_gt(this_points, batch_dict, batch_idx)
 
 
-def analyze(batch_dict, batch_idx=0):
+def analyze(batch_dict):
     def eval(this_points_mask, this_flag_of_pts):
         points_num = this_points_mask.sum().item()
         tp = (this_points_mask & this_flag_of_pts).sum().item()
@@ -69,30 +71,37 @@ def analyze(batch_dict, batch_idx=0):
         return points_num, recall, precision, f1
 
     batch_size, height, width = batch_dict['seg_pred'].shape
-    flag_of_pts = batch_dict['flag_of_pts']
-    points = batch_dict['points']
-    ri_indices = batch_dict['ri_indices']
-    batch_points_mask = points[:, 0] == batch_idx
-    this_ri_indices = ri_indices[batch_points_mask, :]
-    this_ri_indexes = (this_ri_indices[:, 1] * width + this_ri_indices[:, 2]).long()
-    # target
-    this_flag_of_pts = flag_of_pts[batch_points_mask, 1].bool()
-    this_seg_mask = batch_dict['seg_pred'][batch_idx]
-    result = []
-    for i in range(10):
-        threshold = i / 10
-        cur_seg_mask = this_seg_mask >= threshold
-        cur_seg_mask = cur_seg_mask.flatten()
-        # predict
-        this_points_mask = torch.gather(cur_seg_mask, dim=0, index=this_ri_indexes).bool()
-        points_num, recall, precision, f1 = eval(this_points_mask, this_flag_of_pts)
-        result.append((threshold, points_num, recall, precision, f1))
+    batch_result = np.zeros((batch_size,11,5))
+    for batch_idx in range(batch_size):
+        flag_of_pts = batch_dict['flag_of_pts']
+        points = batch_dict['points']
+        ri_indices = batch_dict['ri_indices']
+        batch_points_mask = points[:, 0] == batch_idx
+        this_ri_indices = ri_indices[batch_points_mask, :]
+        this_ri_indexes = (this_ri_indices[:, 1] * width + this_ri_indices[:, 2]).long()
+        # target
+        this_flag_of_pts = flag_of_pts[batch_points_mask, 1].bool()
+        this_seg_mask = batch_dict['seg_pred'][batch_idx]
+        # result = []
+        for i in range(10):
+            threshold = i / 10
+            cur_seg_mask = this_seg_mask >= threshold
+            cur_seg_mask = cur_seg_mask.flatten()
+            # predict
+            this_points_mask = torch.gather(cur_seg_mask, dim=0, index=this_ri_indexes).bool()
+            points_num, recall, precision, f1 = eval(this_points_mask, this_flag_of_pts)
+            batch_result[batch_idx,i] = threshold, points_num, recall, precision, f1
+            # result.append((threshold, points_num, recall, precision, f1))
 
-    this_range_mask = batch_dict['range_mask'][batch_idx]
-    this_range_mask = this_range_mask.flatten()
-    this_points_mask = torch.gather(this_range_mask, dim=0, index=this_ri_indexes).bool()
-    points_num, recall, precision, f1 = eval(this_points_mask, this_flag_of_pts)
-    result.append((1, points_num, recall, precision, f1))
+        this_range_mask = batch_dict['range_mask'][batch_idx]
+        this_range_mask = this_range_mask.flatten()
+        this_points_mask = torch.gather(this_range_mask, dim=0, index=this_ri_indexes).bool()
+        points_num, recall, precision, f1 = eval(this_points_mask, this_flag_of_pts)
+        batch_result[batch_idx,10] = 1, points_num, recall, precision, f1
+        # result.append((1, points_num, recall, precision, f1))
+        # batch_result.append(result)
+    import pudb
+    pudb.set_trace()
     print("threshold    points_num    recall    precision    f1")
-    for res in result:
-        print("%9.2f    %8.2f    %6.2f    %9.2f    %5.2f" % res)
+    for i in range(11):
+        print("%9.2f    %8.2f    %6.2f    %9.2f    %5.2f" % tuple(batch_result.mean(axis=0)[i].tolist()))
--- pcdet/models/seg_heads/map_to_point_cloud/point_gather.py
+++ pcdet/models/seg_heads/map_to_point_cloud/point_gather.py
@@ -44,8 +44,7 @@ class PointGather(nn.Module):
         foreground_voxels = []
         foreground_voxel_coords = []
         foreground_voxel_num_points = []
-        import pudb
-        pudb.set_trace()
+
         analyze(batch_dict)
 
         for batch_idx in range(batch_size):
