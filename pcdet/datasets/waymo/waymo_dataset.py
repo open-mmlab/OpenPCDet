d76af97c85a8bb2a8b0cfd7022f86849148e6ac0
MengAaron <871628893@qq.com>
2021-04-28 19:59:16 +0800

    fix

--- pcdet/datasets/waymo/waymo_dataset.py
+++ pcdet/datasets/waymo/waymo_dataset.py
@@ -195,12 +195,12 @@ class WaymoDataset(DatasetTemplate):
                 'range_image_shape': self.range_config.get('RANGE_IMAGE_SHAPE', [64, 2650])
             })
             # add key 'range_image'(C, H, W), 'range_mask'(H, W), 'ri_indices'(N, 2) to data_dict, 2 means H, W
-            data_dict = waymo_utils.convert_point_cloud_to_range_image(data_dict, self.training)
+            # data_dict = waymo_utils.convert_point_cloud_to_range_image(data_dict, self.training)\
+            data_dict = waymo_utils.test(data_dict)
             points_feature_num = data_dict['points'].shape[1]
             data_dict['points'] = np.concatenate((data_dict['points'], data_dict['ri_indices']), axis=1)
             data_dict = self.prepare_data(data_dict=data_dict, augment=False)
             data_dict['points'] = data_dict['points'][:, :points_feature_num]
-            # data_dict = waymo_utils.test(data_dict)
             data_dict.pop('beam_inclination_range', None)
             data_dict.pop('extrinsic', None)
             data_dict.pop('range_image_shape', None)
