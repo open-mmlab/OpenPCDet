8547e7137f88db4f6876cafd08d0b3b035887cb9
MengAaron <871628893@qq.com>
2021-04-29 16:05:42 +0800

    fix

--- pcdet/models/seg_heads/map_to_point_cloud/point_gather.py
+++ pcdet/models/seg_heads/map_to_point_cloud/point_gather.py
@@ -44,9 +44,9 @@ class PointGather(nn.Module):
         foreground_voxels = []
         foreground_voxel_coords = []
         foreground_voxel_num_points = []
-        # import pudb
-        # pudb.set_trace()
-        # analyze(batch_dict)
+        import pudb
+        pudb.set_trace()
+        analyze(batch_dict)
 
         for batch_idx in range(batch_size):
             this_range_features = range_features[batch_idx].reshape((height * width, -1))
