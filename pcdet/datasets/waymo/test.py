a8f4ab1725a12dd9ce87cc0d63d90d3cd1903583
MengAaron <871628893@qq.com>
2021-05-10 15:59:58 +0800

    fix

--- pcdet/datasets/waymo/test.py
+++ pcdet/datasets/waymo/test.py
@@ -1,75 +1,112 @@
 import numpy as np
 # from waymo_utils import plot_pointcloud
 import mayavi.mlab as mlab
+import matplotlib.pyplot as plt
 
 
-def plot_pointcloud(pointcloud, vals='height'):
-    import mayavi.mlab as mlab
+def plot_bev(pointcloud, res=(0.1, 0.1, 2), pc_range=(-80, -80, -10.0, 80, 80, 10.0)):
     print(pointcloud.shape)
     print(type(pointcloud))
     x = pointcloud[:, 0]  # x position of point
-    xmin = np.amin(x, axis=0)
-    xmax = np.amax(x, axis=0)
     y = pointcloud[:, 1]  # y position of point
-    ymin = np.amin(y, axis=0)
-    ymax = np.amax(y, axis=0)
     z = pointcloud[:, 2]  # z position of point
-    zmin = np.amin(z, axis=0)
-    zmax = np.amax(z, axis=0)
+    r = pointcloud[:, 3]
+    xmin = max(np.amin(x, axis=0),pc_range[0])
+    xmax = min(np.amax(x, axis=0),pc_range[3])
+
+    ymin = max(np.amin(y, axis=0),pc_range[1])
+    ymax = min(np.amax(y, axis=0),pc_range[4])
+
+    zmin = max(np.amin(z, axis=0),pc_range[2])
+    zmax = min(np.amax(z, axis=0),pc_range[5])
     print(xmin, xmax, ymin, ymax, zmin, zmax)
     d = np.sqrt(x ** 2 + y ** 2)  # Map Distance from sensor
 
-    if vals == "height":
-        col = z
-    else:
-        col = d
-    fig = mlab.figure(bgcolor=(0, 0, 0), size=(640, 500))
-    mlab.points3d(x, y, z,
-                  col,  # Values used for Color
-                  mode="point",
-                  # 灰度图的伪彩映射
-                  colormap='spectral',  # 'bone', 'copper', 'gnuplot'
-                  # color=(0, 1, 0),   # Used a fixed (r,g,b) instead
-                  figure=fig,
-                  )
-    # 绘制原点
-    mlab.points3d(0, 0, 0, color=(1, 1, 1), mode="sphere", scale_factor=1)
-    # 绘制坐标
-    axes = np.array(
-        [[20.0, 0.0, 0.0, 0.0], [0.0, 20.0, 0.0, 0.0], [0.0, 0.0, 20.0, 0.0]],
-        dtype=np.float64,
-    )
-    # x轴
-    mlab.plot3d(
-        [0, axes[0, 0]],
-        [0, axes[0, 1]],
-        [0, axes[0, 2]],
-        color=(1, 0, 0),
-        tube_radius=None,
-        figure=fig,
-    )
-    # y轴
-    mlab.plot3d(
-        [0, axes[1, 0]],
-        [0, axes[1, 1]],
-        [0, axes[1, 2]],
-        color=(0, 1, 0),
-        tube_radius=None,
-        figure=fig,
-    )
-    # z轴
-    mlab.plot3d(
-        [0, axes[2, 0]],
-        [0, axes[2, 1]],
-        [0, axes[2, 2]],
-        color=(0, 0, 1),
-        tube_radius=None,
-        figure=fig,
-    )
-    return fig
+
+    # INITIALIZE EMPTY ARRAY - of the dimensions we want
+    x_dim = int((xmax-xmin) / res[0])
+    y_dim = int((ymax-ymin) / res[1])
+    z_dim = int((zmax-zmin) / res[2])
+    print(x_dim,y_dim,z_dim)
+    top = np.zeros([ x_dim + 1,y_dim + 1,z_dim+1], dtype=np.float32)
+
+    # FILTER - To return only indices of points within desired cube
+    # Three filters for: Front-to-back, side-to-side, and height ranges
+    # Note left side is positive y axis in LIDAR coordinates
+    f_filt = np.logical_and(
+        (x > xmin), (x < xmax))
+    s_filt = np.logical_and(
+        (y > ymin), (y < ymax))
+    filt = np.logical_and(f_filt, s_filt)
+
+    for i, height in enumerate(np.arange(zmin, zmax, res[2])):
+        z_filt = np.logical_and((z >= height),
+                                (z < height + res[2]))
+        zfilter = np.logical_and(filt, z_filt)
+        indices = np.argwhere(zfilter).flatten()
+
+        # KEEPERS
+        xi_points = x[indices]
+        yi_points = y[indices]
+        zi_points = z[indices]
+        ri = r[indices]
+        di = d[indices]
+
+        # CONVERT TO PIXEL POSITION VALUES - Based on resolution
+        x_img = (yi_points / res[1]).astype(np.int32)  # x axis is -y in LIDAR
+        y_img = (xi_points / res[0]).astype(np.int32)  # y axis is -x in LIDAR
+
+        # SHIFT PIXELS TO HAVE MINIMUM BE (0,0)
+        # floor & ceil used to prevent anything being rounded to below 0 after
+        # shift
+        x_img -= int(np.floor(ymin / res[1]))
+        y_img -= int(np.floor(xmin / res[0]))
+
+        pixel_values = ri
+
+        # FILL PIXEL VALUES IN IMAGE ARRAY
+        top[y_img,x_img,   i] = zi_points
+
+    # z_filt = np.logical_and((z >= zmin),
+    #                         (z < zmax))
+    # zfilter = np.logical_and(filt, z_filt)
+    # indices = np.argwhere(zfilter).flatten()
+    #
+    # # KEEPERS
+    # xi_points = x[indices]
+    # yi_points = y[indices]
+    # zi_points = z[indices]
+    # ri = r[indices]
+    # di = d[indices]
+    #
+    # # CONVERT TO PIXEL POSITION VALUES - Based on resolution
+    # x_img = (yi_points / res[1]).astype(np.int32)  # x axis is -y in LIDAR
+    # y_img = (xi_points / res[0]).astype(np.int32)  # y axis is -x in LIDAR
+    #
+    # # SHIFT PIXELS TO HAVE MINIMUM BE (0,0)
+    # # floor & ceil used to prevent anything being rounded to below 0 after
+    # # shift
+    # x_img -= int(np.floor(ymin / res[1]))
+    # y_img -= int(np.floor(xmin / res[0]))
+    #
+    # pixel_values = di
+    #
+    # # FILL PIXEL VALUES IN IMAGE ARRAY
+    # top[x_img, y_img] = pixel_values
 
 
-point = np.load('0003.npy')
-# print(point)
-fig = plot_pointcloud(point)
-mlab.show()
+
+    top = (top / np.max(top) * 255).astype(np.uint8)
+    return top
+
+point = np.load('0004.npy')
+fig, ax = plt.subplots(figsize=(10, 10))
+
+top = plot_bev(point)
+top = np.array(top, dtype = np.float32)
+print(top.shape)
+ax.imshow(top, aspect='equal')
+plt.axis('off')
+plt.tight_layout()
+plt.draw()
+plt.show()
--- pcdet/datasets/waymo/waymo_utils.py
+++ pcdet/datasets/waymo/waymo_utils.py
@@ -811,3 +811,74 @@ def _points_in_convex_polygon_3d_jit(
                     ret[i, j] = False
                     break
     return ret
+
+
+def plot_bev(pointcloud, res=(0.1, 0.1, 0.3), pc_range=(-80, -80, -10.0, 80, 80, 10.0)):
+    print(pointcloud.shape)
+    print(type(pointcloud))
+    x = pointcloud[:, 0]  # x position of point
+    y = pointcloud[:, 1]  # y position of point
+    z = pointcloud[:, 2]  # z position of point
+    r = pointcloud[:, 3]
+    xmin = max(np.amin(x, axis=0),pc_range[0])
+    xmax = min(np.amax(x, axis=0),pc_range[3])
+
+    ymin = max(np.amin(y, axis=0),pc_range[1])
+    ymax = min(np.amax(y, axis=0),pc_range[4])
+
+    zmin = max(np.amin(z, axis=0),pc_range[2])
+    zmax = min(np.amax(z, axis=0),pc_range[5])
+    print(xmin, xmax, ymin, ymax, zmin, zmax)
+    d = np.sqrt(x ** 2 + y ** 2)  # Map Distance from sensor
+
+
+    # INITIALIZE EMPTY ARRAY - of the dimensions we want
+    x_dim = int((xmax-xmin) / res[0])
+    y_dim = int((ymax-ymin) / res[1])
+    z_dim = int((zmax-zmin) / res[2])
+    top = np.zeros([y_dim + 1, x_dim + 1, z_dim + 1], dtype=np.float32)
+
+    # FILTER - To return only indices of points within desired cube
+    # Three filters for: Front-to-back, side-to-side, and height ranges
+    # Note left side is positive y axis in LIDAR coordinates
+    f_filt = np.logical_and(
+        (x > pc_range[0]), (x < pc_range[3]))
+    s_filt = np.logical_and(
+        (y > pc_range[1]), (y < pc_range[4]))
+    filt = np.logical_and(f_filt, s_filt)
+
+    for i, height in enumerate(np.arange(zmin, zmax, res[2])):
+        z_filt = np.logical_and((z >= height),
+                                (z < height + res[2]))
+        zfilter = np.logical_and(filt, z_filt)
+        indices = np.argwhere(zfilter).flatten()
+
+        # KEEPERS
+        xi_points = x[indices]
+        yi_points = y[indices]
+        zi_points = z[indices]
+        ri = r[indices]
+        di = d[indices]
+
+        # CONVERT TO PIXEL POSITION VALUES - Based on resolution
+        x_img = (-yi_points / res[1]).astype(np.int32)  # x axis is -y in LIDAR
+        y_img = (-xi_points / res[0]).astype(np.int32)  # y axis is -x in LIDAR
+
+        # SHIFT PIXELS TO HAVE MINIMUM BE (0,0)
+        # floor & ceil used to prevent anything being rounded to below 0 after
+        # shift
+        x_img -= int(np.floor(ymin / res[1]))
+        y_img += int(np.floor(xmin / res[0]))
+
+        # CLIP HEIGHT VALUES - to between min and max heights
+        # pixel_values = zi_points - zmin
+        pixel_values = ri
+
+        # FILL PIXEL VALUES IN IMAGE ARRAY
+        top[y_img, x_img, i] = pixel_values
+
+        # max_intensity = np.max(prs[idx])
+        # top[y_img, x_img, z_dim] = ref_i
+
+    top = (top / np.max(top) * 255).astype(np.uint8)
+    return top
