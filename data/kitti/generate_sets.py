51e90a67c2416727190b177410d9eb8a0cceeb12
Xavier Santos <xavier@ua.pt>
2021-05-10 22:11:59 +0100

    FIX ON GENERATOR

--- data/kitti/generate_sets.py
+++ data/kitti/generate_sets.py
@@ -1,9 +1,10 @@
 import random
-import os, os.path
 import sys
 
-num_frames = int(sys.argv[1])
-dataset_size = len(os.listdir('training/calib'))
+default_size = 7518
+percentage_cut = float(sys.argv[1])
+num_frames = int(default_size * percentage_cut)
+
 
 data = list(range(num_frames))
 random.shuffle(data)
@@ -11,14 +12,14 @@ random.shuffle(data)
 train_data = sorted(data[int(num_frames/2) :])
 val_data = sorted(data[: int(num_frames/2)])
 
-with open("ImageSets/test/train.txt", 'w') as file:
+with open("ImageSets/train.txt", 'w') as file:
     for el in train_data:
         file.write('%06d\n' % el)
 
-with open("ImageSets/test/val.txt", 'w') as file:
+with open("ImageSets/val.txt", 'w') as file:
     for el in val_data:
         file.write('%06d\n' % el)
 
-with open("ImageSets/test/test.txt", 'w') as file:
-    for el in range(dataset_size):
+with open("ImageSets/test.txt", 'w') as file:
+    for el in range(default_size):
         file.write('%06d\n' % el)
