193010ea001279db9245a637cf56fca6b15d0bb8
Xavier Santos <xavier@ua.pt>
2021-05-11 18:32:53 +0100

    FIXED LOGGER

--- pcdet/utils/common_utils.py
+++ pcdet/utils/common_utils.py
@@ -83,17 +83,17 @@ def get_voxel_centers(voxel_coords, downsample_times, voxel_size, point_cloud_ra
     return voxel_centers
 
 
-def create_logger(log_file=None, rank=0, log_level=logging.DEBUG):
+def create_logger(log_file=None, rank=0, log_level=logging.INFO):
     logger = logging.getLogger(__name__)
-    logger.setLevel(log_level if rank == 0 else 'ERROR')
+    logger.setLevel(logging.DEBUG if rank == 0 else 'ERROR')
     formatter = logging.Formatter('%(asctime)s  %(levelname)5s  %(message)s')
     console = logging.StreamHandler()
-    console.setLevel(logging.INFO if rank == 0 else 'ERROR')
+    console.setLevel(log_level if rank == 0 else 'ERROR')
     console.setFormatter(formatter)
     logger.addHandler(console)
     if log_file is not None:
         file_handler = logging.FileHandler(filename=log_file)
-        file_handler.setLevel(logging.INFO if rank == 0 else 'ERROR')
+        file_handler.setLevel(log_level if rank == 0 else 'ERROR')
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
