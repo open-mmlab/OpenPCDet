097a596d19ac22cb684865e9abf1f090cc4a2524
MengAaron <871628893@qq.com>
2021-05-11 15:17:29 +0800

    fix

--- tools/demo.py
+++ tools/demo.py
@@ -145,12 +145,12 @@ def main():
             gt_corners = V.boxes_to_corners_3d(gt_boxes[:30])
             gt_corners = gt_corners.transpose((1, 2, 0))
             for i in range(gt_corners.shape[-1]):
-                x1, x2, x3, x4 = gt_corners[:4, 0, i]
-                y1, y2, y3, y4 = gt_corners[:4, 1, i]
+                x1, x2, x3, x4 = gt_corners[:4, 0,i]
+                y1, y2, y3, y4 = gt_corners[:4, 1,i]
                 plt.plot((x1, y1), (x2, y2), color='yellowgreen', linewidth=2)
                 plt.plot((x1, y1), (x4, y4), color='yellowgreen', linewidth=2)
-                plt.plot((x3, y3), (x2, y2), color='yellowgreen', linewidth=2)
-                plt.plot((x3, y3), (x4, y4), color='yellowgreen', linewidth=2)
+                # plt.plot((x3, y3), (x2, y2), color='yellowgreen', linewidth=2)
+                # plt.plot((x3, y3), (x4, y4), color='yellowgreen', linewidth=2)
             plt.show()
 
     logger.info('Demo done.')
