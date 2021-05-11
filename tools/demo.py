5a06075d0e7732daaa4ed995678e28fb3c00b665
MengAaron <871628893@qq.com>
2021-05-11 15:41:25 +0800

    fix

--- tools/demo.py
+++ tools/demo.py
@@ -134,9 +134,9 @@ def main():
             V.draw_scenes(
                 points=data_dict['points'][:, 1:4],
                 gt_boxes=data_dict.get('gt_boxes', None)[0],
-                ref_boxes=pred_dicts[0]['pred_boxes'][mask],
-                ref_scores=pred_dicts[0]['pred_scores'][mask],
-                ref_labels=pred_dicts[0]['pred_labels'][mask]
+                ref_boxes=pred_dicts[0]['pred_boxes'][mask][:len(data_dict.get('gt_boxes', None)[0])],
+                ref_scores=pred_dicts[0]['pred_scores'][mask][:len(data_dict.get('gt_boxes', None)[0])],
+                ref_labels=pred_dicts[0]['pred_labels'][mask][:len(data_dict.get('gt_boxes', None)[0])]
             )
             mlab.show(stop=True)
             points = data_dict['points'][:, 1:4].cpu().numpy()
