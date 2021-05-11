49514238edfda092653d35c6c1d4c087d5f64d01
MengAaron <871628893@qq.com>
2021-05-11 14:01:57 +0800

    fix

--- tools/demo.py
+++ tools/demo.py
@@ -123,6 +123,8 @@ def main():
             data_dict = demo_dataset.collate_batch([data_dict])
             load_data_to_gpu(data_dict)
             pred_dicts, _ = model.forward(data_dict)
+            import pudb
+            pudb.set_trace()
 
             V.draw_scenes(
                 points=data_dict['points'][:, 1:4],
