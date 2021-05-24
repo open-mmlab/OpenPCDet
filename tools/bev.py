d594b387df61b99ea1d63c2c94d1981022c132e2
MengAaron <871628893@qq.com>
2021-05-24 16:44:42 +0800

    fix

--- tools/bev.py
+++ tools/bev.py
@@ -120,8 +120,6 @@ def transform_to_img(xmin, xmax, ymin, ymax, pc_range, res=0.1):
 
 def draw_boxes(ax, boxes,pc_range, color='green'):
     corners = V.boxes_to_corners_3d(boxes)
-    import pudb
-    pudb.set_trace()
     # corners = corners.transpose((1, 2, 0))
     for o in range(len(boxes)):
         x1, x2, x3, x4 = corners[o][:4, 0]
