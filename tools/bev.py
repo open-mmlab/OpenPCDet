6668ccedfa5a7bdf83fba7a47bbdee9d5c959e11
MengAaron <871628893@qq.com>
2021-05-24 20:21:57 +0800

    fix

--- tools/bev.py
+++ tools/bev.py
@@ -227,7 +227,7 @@ def main():
             ax.imshow(top, aspect='equal', cmap='gray')
             # import pudb
             # pudb.set_trace()
-            draw_boxes(ax, data_dict.get('gt_boxes', None)[0].cpu().numpy(), pc_range, color='blue')
+            # draw_boxes(ax, data_dict.get('gt_boxes', None)[0].cpu().numpy(), pc_range, color='blue')
             draw_boxes(ax, pred_dicts[0]['pred_boxes'][mask2][:100].cpu().numpy(), pc_range, color='red')
             plt.axis('off')
             plt.tight_layout()
