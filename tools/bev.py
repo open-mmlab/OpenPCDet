4711bca9898bf9bd366d11c77590369dc5711a04
MengAaron <871628893@qq.com>
2021-05-24 16:22:31 +0800

    fix

--- tools/bev.py
+++ tools/bev.py
@@ -13,6 +13,7 @@ from pcdet.datasets import DatasetTemplate
 from pcdet.models import build_network, load_data_to_gpu
 from pcdet.utils import common_utils
 from visual_utils import visualize_utils as V
+import matplotlib.lines as lines
 import matplotlib.pyplot as plt
 
 
@@ -108,6 +109,52 @@ def parse_config():
     return args, cfg
 
 
+def transform_to_img(xmin, xmax, ymin, ymax, pc_range, res=0.1):
+    xmin_img = -ymax / res - pc_range[1] / res
+    xmax_img = -ymin / res - pc_range[4] / res
+    ymin_img = -xmax / res + pc_range[0] / res
+    ymax_img = -xmin / res + pc_range[3] / res
+
+    return xmin_img, xmax_img, ymin_img, ymax_img
+
+
+def draw_boxes(ax, boxes,pc_range, color='green'):
+    corners = V.boxes_to_corners_3d(boxes)
+    corners = corners.transpose((1, 2, 0))
+    for o in range(len(boxes)):
+        x1, x2, x3, x4 = corners[o][:4, 0]
+        y1, y2, y3, y4 = corners[o][:4, 1]
+
+        x1, x2, y1, y2 = transform_to_img(x1, x2, y1, y2,pc_range)
+        x3, x4, y3, y4 = transform_to_img(x3, x4, y3, y4,pc_range)
+        ps = []
+        polygon = np.zeros([5, 2], dtype=np.float32)
+        polygon[0, 0] = x1
+        polygon[1, 0] = x2
+        polygon[2, 0] = x3
+        polygon[3, 0] = x4
+        polygon[4, 0] = x1
+
+        polygon[0, 1] = y1
+        polygon[1, 1] = y2
+        polygon[2, 1] = y3
+        polygon[3, 1] = y4
+        polygon[4, 1] = y1
+
+        line1 = [(x1, y1), (x2, y2)]
+        line2 = [(x2, y2), (x3, y3)]
+        line3 = [(x3, y3), (x4, y4)]
+        line4 = [(x4, y4), (x1, y1)]
+        (line1_xs, line1_ys) = zip(*line1)
+        (line2_xs, line2_ys) = zip(*line2)
+        (line3_xs, line3_ys) = zip(*line3)
+        (line4_xs, line4_ys) = zip(*line4)
+        ax.add_line(lines.Line2D(line1_xs, line1_ys, linewidth=1, color=color))
+        ax.add_line(lines.Line2D(line2_xs, line2_ys, linewidth=1, color=color))
+        ax.add_line(lines.Line2D(line3_xs, line3_ys, linewidth=1, color=color))
+        ax.add_line(lines.Line2D(line4_xs, line4_ys, linewidth=1, color=color))
+
+
 def main():
     args, cfg = parse_config()
     logger = common_utils.create_logger()
@@ -122,6 +169,8 @@ def main():
     model.load_params_from_file(filename=args.ckpt, logger=logger, to_cpu=True)
     model.cuda()
     model.eval()
+    pc_range = cfg.DATA_CONFIG.POINT_CLOUD_RANGE
+    res = 0.1
     with torch.no_grad():
         for idx, data_dict in enumerate(demo_dataset):
             logger.info(f'Visualized sample index: \t{idx + 1}')
@@ -136,36 +185,66 @@ def main():
             mask2 = (mask2 < 20) & (mask2 > 0.05)
             mask2 = mask2 & mask
 
-
-
-            # V.draw_scenes(
-            #     points=data_dict['points'][:, 1:4],
-            #     gt_boxes=data_dict.get('gt_boxes', None)[0],
-            #     ref_boxes=pred_dicts[0]['pred_boxes'][mask][:len(data_dict.get('gt_boxes', None)[0])],
-            #     ref_scores=pred_dicts[0]['pred_scores'][mask][:len(data_dict.get('gt_boxes', None)[0])],
-            #     ref_labels=pred_dicts[0]['pred_labels'][mask][:len(data_dict.get('gt_boxes', None)[0])]
-            # )
-            # mlab.show(stop=True)
             points = data_dict['points'][:, 1:4].cpu().numpy()
-            rgba_colors = np.zeros((points.shape[0], 4))
-            rgba_colors[:, 2] = 1
-            rgba_colors[:, 3] = points[:, 2]
-            plt.scatter(points[:, 0], points[:, 1], s=0.5, color=rgba_colors[:, :3])
+            x_points = points[:, 0]
+            y_points = points[:, 1]
+            z_points = points[:, 2]
+            reflectance = points[:, 3]
 
-            gt_boxes = data_dict.get('gt_boxes', None)[0].cpu().numpy()
-            gt_corners = V.boxes_to_corners_3d(gt_boxes)
-            gt_corners = gt_corners.transpose((1, 2, 0))
-            x1, x2, x3, x4 = gt_corners[:4, 0]
-            y1, y2, y3, y4 = gt_corners[:4, 1]
-            plt.plot((x1, x2, x3, x4, x1), (y1, y2, y3, y4, y1), color='yellowgreen', linewidth=2)
-            pred_boxes = pred_dicts[0]['pred_boxes'][mask2].cpu().numpy()
-            pred_corners = V.boxes_to_corners_3d(pred_boxes[:100])
-            pred_corners = pred_corners.transpose((1, 2, 0))
-            x1, x2, x3, x4 = pred_corners[:4, 0]
-            y1, y2, y3, y4 = pred_corners[:4, 1]
-            plt.plot((x1, x2, x3, x4, x1), (y1, y2, y3, y4, y1), color='red', linewidth=2)
-            plt.savefig('bev-%d-rsn.png'%idx)
-            plt.clf()
+            # INITIALIZE EMPTY ARRAY - of the dimensions we want
+            x_max = int((pc_range[3] - pc_range[0]) / res)
+            y_max = int((pc_range[2] - pc_range[1]) / res)
+            # top = np.zeros([y_max+1, x_max+1, z_max+1], dtype=np.float32)
+            top = np.ones([y_max + 1, x_max + 1], dtype=np.float32)
+
+            # FILTER - To return only indices of points within desired cube
+            # Three filters for: Front-to-back, side-to-side, and height ranges
+            # Note left side is positive y axis in LIDAR coordinates
+            f_filt = np.logical_and((x_points > pc_range[0]), (x_points < pc_range[3]))
+            s_filt = np.logical_and((y_points > -pc_range[1]), (y_points < -pc_range[0]))
+            filt = np.logical_and(f_filt, s_filt)
+            indices = np.argwhere(filt).flatten()
+            xi_points = x_points[indices]
+            yi_points = y_points[indices]
+            # CONVERT TO PIXEL POSITION VALUES - Based on resolution
+            x_img = (-yi_points / res).astype(np.int32)  # x axis is -y in LIDAR
+            y_img = (-xi_points / res).astype(np.int32)  # y axis is -x in LIDAR
+
+            # SHIFT PIXELS TO HAVE MINIMUM BE (0,0)
+            # floor & ceil used to prevent anything being rounded to below 0 after
+            # shift
+            x_img -= int(np.floor(pc_range[0] / res))
+            y_img += int(np.floor(pc_range[2] / res))
+            # FILL PIXEL VALUES IN IMAGE ARRAY
+            # top[y_img, x_img, i] = pixel_values
+            top[y_img, x_img] = 0
+            top = (top / np.max(top) * 255).astype(np.uint8)
+            fig, ax = plt.subplots(figsize=(10, 10))
+            ax.imshow(top, aspect='equal', cmap='gray')
+            draw_boxes(ax, data_dict.get('gt_boxes', None)[0].cpu().numpy(),pc_range)
+            draw_boxes(ax, pred_dicts[0]['pred_boxes'][mask2][:100].cpu().numpy(), pc_range)
+            plt.axis('off')
+            plt.tight_layout()
+            plt.savefig('result/bev-%d-rrcnn.png'%idx)
+            # rgba_colors = np.zeros((points.shape[0], 4))
+            # rgba_colors[:, 2] = 1
+            # rgba_colors[:, 3] = points[:, 2]
+            # plt.scatter(points[:, 0], points[:, 1], s=0.5, color=rgba_colors[:, :3])
+            #
+            # gt_boxes = data_dict.get('gt_boxes', None)[0].cpu().numpy()
+            # gt_corners = V.boxes_to_corners_3d(gt_boxes)
+            # gt_corners = gt_corners.transpose((1, 2, 0))
+            # x1, x2, x3, x4 = gt_corners[:4, 0]
+            # y1, y2, y3, y4 = gt_corners[:4, 1]
+            # plt.plot((x1, x2, x3, x4, x1), (y1, y2, y3, y4, y1), color='yellowgreen', linewidth=2)
+            # pred_boxes = pred_dicts[0]['pred_boxes'][mask2].cpu().numpy()
+            # pred_corners = V.boxes_to_corners_3d(pred_boxes[:100])
+            # pred_corners = pred_corners.transpose((1, 2, 0))
+            # x1, x2, x3, x4 = pred_corners[:4, 0]
+            # y1, y2, y3, y4 = pred_corners[:4, 1]
+            # plt.plot((x1, x2, x3, x4, x1), (y1, y2, y3, y4, y1), color='red', linewidth=2)
+            # plt.savefig('bev-%d-rsn.png'%idx)
+            # plt.clf()
             # plt.show()
 
     logger.info('Demo done.')
