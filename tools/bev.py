24c24854dcbfa41e0eb5be715eab466de23bc034
MengAaron <871628893@qq.com>
2021-05-24 16:29:02 +0800

    fix

--- tools/bev.py
+++ tools/bev.py
@@ -186,6 +186,8 @@ def main():
             mask2 = mask2 & mask
 
             points = data_dict['points'][:, 1:4].cpu().numpy()
+            import pudb
+            pudb.set_trace()
             x_points = points[:, 0]
             y_points = points[:, 1]
             # z_points = points[:, 2]
