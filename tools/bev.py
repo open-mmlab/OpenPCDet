2e344fb55195d8e8638d5bc128848fea1eb6cc71
MengAaron <871628893@qq.com>
2021-05-25 10:14:50 +0800

    fix

--- tools/bev.py
+++ tools/bev.py
@@ -228,6 +228,8 @@ def main():
             # import pudb
             # pudb.set_trace()
             # draw_boxes(ax, data_dict.get('gt_boxes', None)[0].cpu().numpy(), pc_range, color='blue')
+            import pudb
+            pudb.set_trace()
             draw_boxes(ax, pred_dicts[0]['pred_boxes'][mask2][:100].cpu().numpy(), pc_range, color='red')
             plt.axis('off')
             plt.tight_layout()
