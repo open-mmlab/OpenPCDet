55d4bf74108bff9358197566ff0eeabb293f1346
MengAaron <871628893@qq.com>
2021-05-25 15:03:23 +0800

    fix

--- pcdet/models/roi_heads/__init__.py
+++ pcdet/models/roi_heads/__init__.py
@@ -2,10 +2,12 @@ from .partA2_head import PartA2FCHead
 from .pointrcnn_head import PointRCNNHead
 from .pvrcnn_head import PVRCNNHead
 from .roi_head_template import RoIHeadTemplate
+from .rrcnn_head import RRCNNHead
 
 __all__ = {
     'RoIHeadTemplate': RoIHeadTemplate,
     'PartA2FCHead': PartA2FCHead,
     'PVRCNNHead': PVRCNNHead,
-    'PointRCNNHead': PointRCNNHead
+    'PointRCNNHead': PointRCNNHead,
+    'RRCNNHead': RRCNNHead,
 }
