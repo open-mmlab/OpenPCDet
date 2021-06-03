5b38ee8f3000c4020874709c653eab4c33073e85
Jiajun Deng <dengjj@mail.ustc.edu.cn>
2021-06-03 01:22:44 -0400

    add voxel query

--- pcdet/ops/pointnet2/pointnet2_stack/src/voxel_query_gpu.cu
+++ pcdet/ops/pointnet2/pointnet2_stack/src/voxel_query_gpu.cu
@@ -1,122 +1,110 @@
-/*
-Stacked-batch-data version of point grouping, modified from the original implementation of official PointNet++ codes.
-Written by Shaoshuai Shi
-All Rights Reserved 2019-2020.
-*/
-
-
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <curand_kernel.h>
 
+#include "voxel_query_gpu.h"
 #include "cuda_utils.h"
-#include "group_points_gpu.h"
 
 
-__global__ void group_points_grad_kernel_stack(int B, int M, int C, int N, int nsample,
-    const float *grad_out, const int *idx, const int *idx_batch_cnt, const int *features_batch_cnt, float *grad_features) {
-    // :param grad_out: (M1 + M2 ..., C, nsample) tensor of the gradients of the output from forward
-    // :param idx: (M1 + M2 ..., nsample) tensor containing the indicies of features to group with
-    // :param idx_batch_cnt: (batch_size) [M1 + M2 ...] tensor containing the indicies of features to group with
-    // :param features_batch_cnt: (batch_size) [N1 + N2 ...] tensor containing the indicies of features to group with
-    // :return:
-    //     grad_features: (N1 + N2 ..., C) gradient of the features
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-    int sample_idx = index % nsample;
-    int C_idx = (index / nsample) % C;
-    int pt_idx = (index / nsample / C);
+__global__ void voxel_query_kernel_stack(int M, int R1, int R2, int R3, int nsample, 
+            float radius, int z_range, int y_range, int x_range, const float *new_xyz, 
+            const float *xyz, const int *new_coords, const int *point_indices, int *idx) {
+    // :param new_coords: (M1 + M2 ..., 4) centers of the ball query
+    // :param point_indices: (B, Z, Y, X)
+    // output:
+    //      idx: (M1 + M2, nsample)
+    int pt_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (pt_idx >= M) return;
+    
+    new_xyz += pt_idx * 3;
+    new_coords += pt_idx * 4;
+    idx += pt_idx * nsample;
 
-    if (pt_idx >= M || C_idx >= C || sample_idx >= nsample) return;
+    curandState state;
+    curand_init(pt_idx, 0, 0, &state);
+    
+    float radius2 = radius * radius;
+    float new_x = new_xyz[0];
+    float new_y = new_xyz[1];
+    float new_z = new_xyz[2];
 
-    int bs_idx = 0, pt_cnt = idx_batch_cnt[0];
-    for (int k = 1; k < B; k++){
-        if (pt_idx < pt_cnt) break;
-        pt_cnt += idx_batch_cnt[k];
-        bs_idx = k;
+    int batch_idx = new_coords[0];
+    int new_coords_z = new_coords[1];
+    int new_coords_y = new_coords[2];
+    int new_coords_x = new_coords[3];
+    
+    int cnt = 0;
+    int cnt2 = 0;
+    // for (int dz = -1*z_range; dz <= z_range; ++dz) {
+    for (int dz = -1*z_range; dz <= z_range; ++dz) {
+        int z_coord = new_coords_z + dz;
+        if (z_coord < 0 || z_coord >= R1) continue;
+
+        for (int dy = -1*y_range; dy <= y_range; ++dy) {
+            int y_coord = new_coords_y + dy;
+            if (y_coord < 0 || y_coord >= R2) continue;
+
+            for (int dx = -1*x_range; dx <= x_range; ++dx) {
+                int x_coord = new_coords_x + dx;
+                if (x_coord < 0 || x_coord >= R3) continue;
+
+                int index = batch_idx * R1 * R2 * R3 + \
+                            z_coord * R2 * R3 + \
+                            y_coord * R3 + \
+                            x_coord;
+                int neighbor_idx = point_indices[index];
+                if (neighbor_idx < 0) continue;
+                
+                float x_per = xyz[neighbor_idx*3 + 0];
+                float y_per = xyz[neighbor_idx*3 + 1];
+                float z_per = xyz[neighbor_idx*3 + 2];
+
+                float dist2 = (x_per - new_x) * (x_per - new_x) + (y_per - new_y) * (y_per - new_y) + (z_per - new_z) * (z_per - new_z);
+
+                if (dist2 > radius2) continue;
+                
+                ++cnt2;
+
+                if (cnt < nsample) {
+                    if (cnt == 0) {
+                        for (int l = 0; l < nsample; ++l) {
+                            idx[l] = neighbor_idx;
+                        }
+                    }
+                    idx[cnt] = neighbor_idx;
+                    ++cnt;
+                }
+                // else {
+                //     float rnd = curand_uniform(&state);
+                //     if (rnd < (float(nsample) / cnt2)) {
+                //         int insertidx = ceilf(curand_uniform(&state) * nsample) - 1;
+                //         idx[insertidx] = neighbor_idx;
+                //     }
+                // }
+            }
+        }
     }
-
-    int features_batch_start_idx = 0;
-    for (int k = 0; k < bs_idx; k++) features_batch_start_idx += features_batch_cnt[k];
-
-    grad_out += pt_idx * C * nsample + C_idx * nsample + sample_idx;
-    idx += pt_idx * nsample + sample_idx;
-    grad_features += (features_batch_start_idx + idx[0]) * C + C_idx;
-
-    atomicAdd(grad_features, grad_out[0]);
+   if (cnt == 0) idx[0] = -1;
 }
 
-void group_points_grad_kernel_launcher_stack(int B, int M, int C, int N, int nsample,
-    const float *grad_out, const int *idx, const int *idx_batch_cnt, const int *features_batch_cnt, float *grad_features) {
-    // :param grad_out: (M1 + M2 ..., C, nsample) tensor of the gradients of the output from forward
-    // :param idx: (M1 + M2 ..., nsample) tensor containing the indicies of features to group with
-    // :param idx_batch_cnt: (batch_size) [M1 + M2 ...] tensor containing the indicies of features to group with
-    // :param features_batch_cnt: (batch_size) [N1 + N2 ...] tensor containing the indicies of features to group with
-    // :return:
-    //     grad_features: (N1 + N2 ..., C) gradient of the features
+
+void voxel_query_kernel_launcher_stack(int M, int R1, int R2, int R3, int nsample,
+    float radius, int z_range, int y_range, int x_range, const float *new_xyz, 
+    const float *xyz, const int *new_coords, const int *point_indices, int *idx) {
+    // :param new_coords: (M1 + M2 ..., 4) centers of the voxel query
+    // :param point_indices: (B, Z, Y, X) 
+    // output:
+    //      idx: (M1 + M2, nsample)
 
     cudaError_t err;
-    // dim3 blocks(DIVUP(npoints * nsample, THREADS_PER_BLOCK), c, b);  // blockIdx.x(col), blockIdx.y(row)
-    dim3 blocks(DIVUP(M * C * nsample, THREADS_PER_BLOCK));  // blockIdx.x(col), blockIdx.y(row)
+
+    dim3 blocks(DIVUP(M, THREADS_PER_BLOCK));  // blockIdx.x(col), blockIdx.y(row)
     dim3 threads(THREADS_PER_BLOCK);
 
-    group_points_grad_kernel_stack<<<blocks, threads>>>(B, M, C, N, nsample, grad_out, idx, idx_batch_cnt, features_batch_cnt, grad_features);
-
-    err = cudaGetLastError();
-    if (cudaSuccess != err) {
-        fprintf(stderr, "CUDA kernel failed : %s\n", cudaGetErrorString(err));
-        exit(-1);
-    }
-}
-
-
-__global__ void group_points_kernel_stack(int B, int M, int C, int nsample,
-    const float *features, const int *features_batch_cnt, const int *idx, const int *idx_batch_cnt, float *out) {
-    // :param features: (N1 + N2 ..., C) tensor of features to group
-    // :param features_batch_cnt: (batch_size) [N1 + N2 ...] tensor containing the indicies of features to group with
-    // :param idx: (M1 + M2 ..., nsample) tensor containing the indicies of features to group with
-    // :param idx_batch_cnt: (batch_size) [M1 + M2 ...] tensor containing the indicies of features to group with
-    // :return:
-    //     output: (M1 + M2, C, nsample) tensor
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-    int sample_idx = index % nsample;
-    int C_idx = (index / nsample) % C;
-    int pt_idx = (index / nsample / C);
-
-    if (pt_idx >= M || C_idx >= C || sample_idx >= nsample) return;
-
-    int bs_idx = 0, pt_cnt = idx_batch_cnt[0];
-    for (int k = 1; k < B; k++){
-        if (pt_idx < pt_cnt) break;
-        pt_cnt += idx_batch_cnt[k];
-        bs_idx = k;
-    }
-
-    int features_batch_start_idx = 0;
-    for (int k = 0; k < bs_idx; k++) features_batch_start_idx += features_batch_cnt[k];
-    features += features_batch_start_idx * C;
-
-    idx += pt_idx * nsample + sample_idx;
-    int in_idx = idx[0] * C + C_idx;
-    int out_idx = pt_idx * C * nsample + C_idx * nsample + sample_idx;
-
-    out[out_idx] = features[in_idx];
-}
-
-
-void group_points_kernel_launcher_stack(int B, int M, int C, int nsample,
-    const float *features, const int *features_batch_cnt, const int *idx, const int *idx_batch_cnt, float *out) {
-    // :param features: (N1 + N2 ..., C) tensor of features to group
-    // :param features_batch_cnt: (batch_size) [N1 + N2 ...] tensor containing the indicies of features to group with
-    // :param idx: (M1 + M2 ..., nsample) tensor containing the indicies of features to group with
-    // :param idx_batch_cnt: (batch_size) [M1 + M2 ...] tensor containing the indicies of features to group with
-    // :return:
-    //     output: (M1 + M2, C, nsample) tensor
-
-    cudaError_t err;
-    dim3 blocks(DIVUP(M * C * nsample, THREADS_PER_BLOCK));  // blockIdx.x(col), blockIdx.y(row)
-    dim3 threads(THREADS_PER_BLOCK);
-
-    group_points_kernel_stack<<<blocks, threads>>>(B, M, C, nsample, features, features_batch_cnt, idx, idx_batch_cnt, out);
+    voxel_query_kernel_stack<<<blocks, threads>>>(M, R1, R2, R3, nsample, radius, z_range, y_range, x_range, new_xyz, xyz, new_coords, point_indices, idx);
     // cudaDeviceSynchronize();  // for using printf in kernel function
+
     err = cudaGetLastError();
     if (cudaSuccess != err) {
         fprintf(stderr, "CUDA kernel failed : %s\n", cudaGetErrorString(err));
