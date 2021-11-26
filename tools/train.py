35df959bacdeb37718c60f36aafae30cbd5be090
Shaoshuai Shi <shaoshuaics@gmail.com>
2021-11-26 21:15:35 +0100

    set to only evaluate last epoch in train.py

--- tools/train.py
+++ tools/train.py
@@ -186,7 +186,7 @@ def main():
     )
     eval_output_dir = output_dir / 'eval' / 'eval_with_train'
     eval_output_dir.mkdir(parents=True, exist_ok=True)
-    args.start_epoch = max(args.epochs - 10, 0)  # Only evaluate the last 10 epochs
+    args.start_epoch = max(args.epochs - 0, 0)  # Only evaluate the last 10 epochs
 
     repeat_eval_ckpt(
         model.module if dist_train else model,
