d9e76fe90779ff03d4520f2e449f67e99ece938d
jihan.yang <jihanyang13@gmail.com>
2021-12-05 13:26:31 +0800

    support recording batch time during training (#697)

--- tools/train_utils/train_utils.py
+++ tools/train_utils/train_utils.py
@@ -3,7 +3,9 @@ import os
 
 import torch
 import tqdm
+import time
 from torch.nn.utils import clip_grad_norm_
+from pcdet.utils import common_utils, commu_utils
 
 
 def train_one_epoch(model, optimizer, train_loader, model_func, lr_scheduler, accumulated_iter, optim_cfg,
@@ -13,14 +15,21 @@ def train_one_epoch(model, optimizer, train_loader, model_func, lr_scheduler, ac
 
     if rank == 0:
         pbar = tqdm.tqdm(total=total_it_each_epoch, leave=leave_pbar, desc='train', dynamic_ncols=True)
+        data_time = common_utils.AverageMeter()
+        batch_time = common_utils.AverageMeter()
+        forward_time = common_utils.AverageMeter()
 
     for cur_it in range(total_it_each_epoch):
+        end = time.time()
         try:
             batch = next(dataloader_iter)
         except StopIteration:
             dataloader_iter = iter(train_loader)
             batch = next(dataloader_iter)
             print('new iters')
+        
+        data_timer = time.time()
+        cur_data_time = data_timer - end
 
         lr_scheduler.step(accumulated_iter)
 
@@ -37,12 +46,31 @@ def train_one_epoch(model, optimizer, train_loader, model_func, lr_scheduler, ac
 
         loss, tb_dict, disp_dict = model_func(model, batch)
 
+        forward_timer = time.time()
+        cur_forward_time = forward_timer - data_timer
+
         loss.backward()
         clip_grad_norm_(model.parameters(), optim_cfg.GRAD_NORM_CLIP)
         optimizer.step()
 
         accumulated_iter += 1
-        disp_dict.update({'loss': loss.item(), 'lr': cur_lr})
+
+        cur_batch_time = time.time() - end
+        # average reduce
+        avg_data_time = commu_utils.average_reduce_value(cur_data_time)
+        avg_forward_time = commu_utils.average_reduce_value(cur_forward_time)
+        avg_batch_time = commu_utils.average_reduce_value(cur_batch_time)
+
+        if rank == 0:
+            data_time.update(avg_data_time)
+            forward_time.update(avg_forward_time)
+            batch_time.update(avg_batch_time)
+
+
+        disp_dict.update({
+            'loss': loss.item(), 'lr': cur_lr, 'd_time': f'{data_time.val:.2f}({data_time.avg:.2f})',
+            'f_time': f'{forward_time.val:.2f}({forward_time.avg:.2f})', 'b_time': f'{batch_time.val:.2f}({batch_time.avg:.2f})'
+        })
 
         # log to console and tensorboard
         if rank == 0:
