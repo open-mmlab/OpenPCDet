3d9b2f7818a8e68845263bba21d8c1341f2df18f
Jiajun Deng <dengjj@mail.ustc.edu.cn>
2022-01-18 01:09:53 +0800

    recover original mask_points_by_range

--- pcdet/utils/common_utils.py
+++ pcdet/utils/common_utils.py
@@ -58,10 +58,8 @@ def rotate_points_along_z(points, angle):
 
 
 def mask_points_by_range(points, limit_range):
-    # mask = (points[:, 0] >= limit_range[0]) & (points[:, 0] <= limit_range[3]) \
-    #        & (points[:, 1] >= limit_range[1]) & (points[:, 1] <= limit_range[4])
-    mask = (points[:, 0] >= limit_range[0]) & (points[:, 0] < limit_range[3]) \
-           & (points[:, 1] >= limit_range[1]) & (points[:, 1] < limit_range[4])
+    mask = (points[:, 0] >= limit_range[0]) & (points[:, 0] <= limit_range[3]) \
+           & (points[:, 1] >= limit_range[1]) & (points[:, 1] <= limit_range[4])
     return mask
 
 
