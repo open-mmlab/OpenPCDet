dd46c13b807ea7fef927849b9b51f9be942f13e7
DaegyuLee <lee.dk@kaist.ac.kr>
2022-04-18 20:08:03 +0900

    update

--- tools/ros_inference.py
+++ tools/ros_inference.py
@@ -164,14 +164,16 @@ def main():
             data_dict = demo_dataset.collate_batch([data_dict])
             load_data_to_gpu(data_dict)
             pred_dicts, _ = model.forward(data_dict)
+            # new_pred_dict = pred_dicts[0]['pred_boxes'][pred_dicts[0]['pred_boxes'][:,0] < 5.0]
 
             print(pred_dicts[0]['pred_boxes'])
+            # print(new_pred_dict)
             # print(data_dict['points'][:, 1:])
-        
-            V.draw_scenes(
-                points=data_dict['points'][:, 1:], ref_boxes=pred_dicts[0]['pred_boxes'],
-                ref_scores=pred_dicts[0]['pred_scores'], ref_labels=pred_dicts[0]['pred_labels']
-            )
+            
+            # V.draw_scenes(
+            #     points=data_dict['points'][:, 1:], ref_boxes=pred_dicts[0]['pred_boxes'],
+            #     ref_scores=pred_dicts[0]['pred_scores'], ref_labels=pred_dicts[0]['pred_labels']
+            # )
 
         r.sleep()
 
