fe62793d9362b5c794724c3eaf83ddd7db7b23ce
Liu Yihua <yihuajack@live.cn>
2022-07-24 14:13:01 +0800

    fix: iou3d build compatibility errors
    Fix #681.
    1. const float -> constexpr float, referring to https://github.com/opencv/opencv/issues/13491 and https://github.com/opencv/opencv/pull/13960
    2. rename check_rect_cross function to resolve name conflicts
    3. change long into int32_t (unsigned long long seems no problem yet)

--- pcdet/ops/iou3d_nms/src/iou3d_nms.cpp
+++ pcdet/ops/iou3d_nms/src/iou3d_nms.cpp
@@ -95,7 +95,7 @@ int nms_gpu(at::Tensor boxes, at::Tensor keep, float nms_overlap_thresh){
 
     int boxes_num = boxes.size(0);
     const float * boxes_data = boxes.data<float>();
-    long * keep_data = keep.data<long>();
+    int32_t * keep_data = keep.data<int32_t>();
 
     const int col_blocks = DIVUP(boxes_num, THREADS_PER_BLOCK_NMS);
 
@@ -107,14 +107,14 @@ int nms_gpu(at::Tensor boxes, at::Tensor keep, float nms_overlap_thresh){
     // unsigned long long *mask_cpu = new unsigned long long [boxes_num * col_blocks];
     std::vector<unsigned long long> mask_cpu(boxes_num * col_blocks);
 
-//    printf("boxes_num=%d, col_blocks=%d\n", boxes_num, col_blocks);
+    // printf("boxes_num=%d, col_blocks=%d\n", boxes_num, col_blocks);
     CHECK_ERROR(cudaMemcpy(&mask_cpu[0], mask_data, boxes_num * col_blocks * sizeof(unsigned long long),
                            cudaMemcpyDeviceToHost));
 
     cudaFree(mask_data);
 
-    unsigned long long remv_cpu[col_blocks];
-    memset(remv_cpu, 0, col_blocks * sizeof(unsigned long long));
+    std::vector<unsigned long long> remv_cpu(col_blocks);
+    memset(&remv_cpu[0], 0, col_blocks * sizeof(unsigned long long));
 
     int num_to_keep = 0;
 
@@ -145,7 +145,7 @@ int nms_normal_gpu(at::Tensor boxes, at::Tensor keep, float nms_overlap_thresh){
 
     int boxes_num = boxes.size(0);
     const float * boxes_data = boxes.data<float>();
-    long * keep_data = keep.data<long>();
+    int32_t * keep_data = keep.data<int32_t>();
 
     const int col_blocks = DIVUP(boxes_num, THREADS_PER_BLOCK_NMS);
 
@@ -157,14 +157,14 @@ int nms_normal_gpu(at::Tensor boxes, at::Tensor keep, float nms_overlap_thresh){
     // unsigned long long *mask_cpu = new unsigned long long [boxes_num * col_blocks];
     std::vector<unsigned long long> mask_cpu(boxes_num * col_blocks);
 
-//    printf("boxes_num=%d, col_blocks=%d\n", boxes_num, col_blocks);
+    // printf("boxes_num=%d, col_blocks=%d\n", boxes_num, col_blocks);
     CHECK_ERROR(cudaMemcpy(&mask_cpu[0], mask_data, boxes_num * col_blocks * sizeof(unsigned long long),
                            cudaMemcpyDeviceToHost));
 
     cudaFree(mask_data);
 
-    unsigned long long remv_cpu[col_blocks];
-    memset(remv_cpu, 0, col_blocks * sizeof(unsigned long long));
+    std::vector<unsigned long long> remv_cpu(col_blocks);
+    memset(&remv_cpu[0], 0, col_blocks * sizeof(unsigned long long));
 
     int num_to_keep = 0;
 
