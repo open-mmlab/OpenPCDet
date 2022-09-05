eb956f4102a851281996a133f3f89f8d7d75a56d
ahmedius2 <ahmedius2@gmail.com>
2022-09-05 17:56:33 +0000

    Enable gpu voxelization

--- pcdet/datasets/processor/data_processor.py
+++ pcdet/datasets/processor/data_processor.py
@@ -22,7 +22,7 @@ class VoxelGeneratorWrapper():
                 from spconv.utils import VoxelGenerator
                 self.spconv_ver = 1
             except:
-                from spconv.utils import Point2VoxelCPU3d as VoxelGenerator
+                from spconv.utils import Point2VoxelGPU3d as VoxelGenerator
                 self.spconv_ver = 2
 
         if self.spconv_ver == 1:
@@ -51,12 +51,12 @@ class VoxelGeneratorWrapper():
                 voxels, coordinates, num_points = voxel_output
         else:
             assert tv is not None, f"Unexpected error, library: 'cumm' wasn't imported properly."
-            voxel_output = self._voxel_generator.point_to_voxel(tv.from_numpy(points))
+            voxel_output = self._voxel_generator.point_to_voxel_hash(tv.from_numpy(points).cuda())
             tv_voxels, tv_coordinates, tv_num_points = voxel_output
             # make copy with numpy(), since numpy_view() will disappear as soon as the generator is deleted
-            voxels = tv_voxels.numpy()
-            coordinates = tv_coordinates.numpy()
-            num_points = tv_num_points.numpy()
+            voxels = tv_voxels.cpu().numpy()
+            coordinates = tv_coordinates.cpu().numpy()
+            num_points = tv_num_points.cpu().numpy()
         return voxels, coordinates, num_points
 
 
