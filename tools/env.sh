cca4116dbdd79cf8dde5c056ed53ece388c00c84
ahmedius2 <ahmedius2@gmail.com>
2022-09-05 16:05:53 +0000

    Added git pull to the env command

--- tools/env.sh
+++ tools/env.sh
@@ -5,6 +5,8 @@ export NUSCENES_MINI_PATH=/root/shared_data/nuscenes/nuscenes-mini/
 export MODELS_PATH=/root/shared_data/models
 export SPLITS_FILE=/root/nuscenes-devkit/python-sdk/nuscenes/utils/splits.py
 
+git pull
+
 mkdir -p ../data/nuscenes
 pushd ../data/nuscenes
 ln -s $NUSCENES_MINI_PATH v1.0-mini
