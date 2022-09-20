8c047fe9a15e4f8d6828867d48c41f9b9cd3fce6
“agent-sgs” <sgshit@163.com>
2022-09-20 13:35:20 +0800

    bug

--- pcdet/models/detectors/__init__.py
+++ pcdet/models/detectors/__init__.py
@@ -11,6 +11,7 @@ from .centerpoint import CenterPoint
 from .pv_rcnn_plusplus import PVRCNNPlusPlus
 from .mppnet import MPPNet
 from .mppnet_e2e import MPPNetE2E
+from .pillarnet import PillarNet
 
 __all__ = {
     'Detector3DTemplate': Detector3DTemplate,
@@ -25,7 +26,8 @@ __all__ = {
     'CenterPoint': CenterPoint,
     'PVRCNNPlusPlus': PVRCNNPlusPlus,
     'MPPNet': MPPNet,
-    'MPPNetE2E': MPPNetE2E
+    'MPPNetE2E': MPPNetE2E,
+    'PillarNet': PillarNet
 }
 
 
