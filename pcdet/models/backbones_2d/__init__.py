cc5f0a57f5bedb1c09053dc43239360608ed4fa3
ahmedius2 <ahmedius@gmail.com>
2022-12-06 02:38:20 +0000

    sbnet backbone integration with fixed mask. performance is not very good but it appears working.

--- pcdet/models/backbones_2d/__init__.py
+++ pcdet/models/backbones_2d/__init__.py
@@ -1,9 +1,8 @@
-from .base_bev_backbone import BaseBEVBackbone
-from .base_bev_backbone_skip import BaseBEVBackboneSkip
+#from .base_bev_backbone import BaseBEVBackbone
+from .base_bev_backbone_sbnet import BaseBEVBackbone
 from .base_bev_backbone_imprecise import BaseBEVBackboneImprecise
 
 __all__ = {
     'BaseBEVBackbone': BaseBEVBackbone,
-    'BaseBEVBackboneSkip': BaseBEVBackboneSkip,
     'BaseBEVBackboneImprecise': BaseBEVBackboneImprecise
 }
