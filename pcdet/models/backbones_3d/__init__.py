8d988a2a3540b1ecb3e54d1c3195989d00863a78
yukang <yukangchen@cse.cuhk.edu.hk>
2023-04-23 21:32:57 +0800

    Add files via upload

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -3,6 +3,7 @@ from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x
 from .spconv_backbone_2d import PillarBackBone8x, PillarRes18BackBone8x
 from .spconv_backbone_focal import VoxelBackBone8xFocal
 from .spconv_backbone_voxelnext import VoxelResBackBone8xVoxelNeXt
+from .spconv_backbone_voxelnext2d import VoxelResBackBone8xVoxelNeXt2D
 from .spconv_unet import UNetV2
 
 __all__ = {
@@ -13,6 +14,7 @@ __all__ = {
     'VoxelResBackBone8x': VoxelResBackBone8x,
     'VoxelBackBone8xFocal': VoxelBackBone8xFocal,
     'VoxelResBackBone8xVoxelNeXt': VoxelResBackBone8xVoxelNeXt,
+    'VoxelResBackBone8xVoxelNeXt2D': VoxelResBackBone8xVoxelNeXt2D,
     'PillarBackBone8x': PillarBackBone8x,
     'PillarRes18BackBone8x': PillarRes18BackBone8x
 }
