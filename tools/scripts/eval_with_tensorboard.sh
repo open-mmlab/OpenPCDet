5eeb86547f01274ce3ddb88480166aa60337ceff
Marc Brede <marc.brede@tum.de>
2023-08-25 10:38:00 +0000

    Update training scripts

--- tools/scripts/eval_with_tensorboard.sh
+++ tools/scripts/eval_with_tensorboard.sh
@@ -1,8 +1,7 @@
-date='put date here'
+date='2023-08-24_13-25-38'
 architecture='pv_rcnn_relation'
 
-tmux send-keys -t 0 "python test.py --cfg ./cfgs/kitti_models/$architecture.yaml --eval_all --extra_tag $date" C-m
+tmux send-keys -t 0 "(cd tools/; python test.py --cfg ./cfgs/kitti_models/$architecture.yaml --eval_all --extra_tag $date)" C-m
 
-sleep 2
-
+tmux new-window -n tensorboard
 tmux send-keys -t 1 "(cd output/cfgs/kitti_models/$architecture/$date/eval/eval_all_default/default; tensorboard dev upload --logdir tensorboard_val --name \"${architecture//-/ } KITTI Evaluation $date\")" C-m
--- tools/scripts/train_with_tensorboard.sh
+++ tools/scripts/train_with_tensorboard.sh
@@ -7,12 +7,9 @@ date=$(date "+%Y-%m-%d_%H-%M-%S")
 architecture="pv_rcnn_relation"
 
 # Sending the training command to tmux session 0
-tmux send-keys -t 0 "python train.py --cfg_file ./cfgs/kitti_models/$architecture.yaml --extra_tag $date" C-m
+tmux send-keys -t 0 "(cd tools/; python train.py --cfg_file ./cfgs/kitti_models/$architecture.yaml --extra_tag $date)" C-m
 
-# Giving the command time to start
-sleep 2
 
-# Navigating to the output directory and starting tensorboard in tmux session 1
-# Note: the architecture name is converted to uppercase with "${architecture^^}"
+tmux new-window -n tensorboard
 tmux send-keys -t 1 "(cd output/cfgs/kitti_models/$architecture/default/; tensorboard dev upload --logdir tensorboard --name \"${architecture^^} KITTI $date\")" C-m
 
