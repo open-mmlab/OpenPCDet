3e75d1d0156e45f752cbabef54d6fe4345157bb7
Marc Brede <marc.brede@tum.de>
2023-08-26 09:36:21 +0000

    Update evaluation and training scripts

--- tools/scripts/eval_with_tensorboard.sh
+++ tools/scripts/eval_with_tensorboard.sh
@@ -1,7 +1,14 @@
-date='2023-08-24_13-25-38'
+#!/bin/bash
+date='2023-08-25_13-03-43'
 architecture='pv_rcnn_relation'
 
-tmux send-keys -t 0 "(cd tools/; python test.py --cfg ./cfgs/kitti_models/$architecture.yaml --eval_all --extra_tag $date)" C-m
+# Create the first tmux session and run a command
+tmux new-session -d -s Session1
+tmux send-keys -t Session1 "(cd tools/; python test.py --cfg ./cfgs/kitti_models/$architecture.yaml --eval_all --extra_tag $date)" C-m
 
-tmux new-window -n tensorboard
-tmux send-keys -t 1 "(cd output/cfgs/kitti_models/$architecture/$date/eval/eval_all_default/default; tensorboard dev upload --logdir tensorboard_val --name \"${architecture//-/ } KITTI Evaluation $date\")" C-m
+sleep 300
+
+# Create the second tmux session and run another command
+tmux new-session -d -s Session2
+tmux send-keys -t Session2 "(cd output/cfgs/kitti_models/$architecture/$date/eval/eval_all_default/default; tensorboard dev upload --logdir tensorboard_val --name \"${architecture//-/ } KITTI Evaluation $date\")" C-m
+
--- tools/scripts/train_with_tensorboard.sh
+++ tools/scripts/train_with_tensorboard.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
-# Getting the current date
 date=$(date "+%Y-%m-%d_%H-%M-%S")
-
-# Defining the architecture variable
 architecture="pv_rcnn_relation"
 
 # Sending the training command to tmux session 0
-tmux send-keys -t 0 "(cd tools/; python train.py --cfg_file ./cfgs/kitti_models/$architecture.yaml --extra_tag $date)" C-m
+tmux new-session -d -s Session1
+tmux send-keys -t Session1 "(cd tools/; python train.py --cfg_file ./cfgs/kitti_models/$architecture.yaml --extra_tag $date)" C-m
 
+sleep 300
 
-tmux new-window -n tensorboard
-tmux send-keys -t 1 "(cd output/cfgs/kitti_models/$architecture/default/; tensorboard dev upload --logdir tensorboard --name \"${architecture^^} KITTI $date\")" C-m
+tmux new-session -d -s Session2
+tmux send-keys -t Session2 "(cd output/cfgs/kitti_models/$architecture/default/; tensorboard dev upload --logdir tensorboard --name \"${architecture^^} KITTI $date\")" C-m
 
