820426d1f5a1dc843d7f70b20acdb25b729d8c12
Marc Brede <marc.brede@tum.de>
2023-08-28 09:36:35 +0000

    git update eval script

--- tools/scripts/eval_with_tensorboard.sh
+++ tools/scripts/eval_with_tensorboard.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-date='2023-08-25_13-03-43'
+date='2023-08-27_11-56-11'
 architecture='pv_rcnn_relation'
 
 # tmux kill-server
 
 # Create the first tmux session and run a command
 tmux new-session -d -s Session1
-tmux send-keys -t Session1 "(cd tools/; python test.py --cfg ./cfgs/kitti_models/$architecture/$date/$architecture.yaml --eval_all --extra_tag $date)" C-m
+tmux send-keys -t Session1 "(cd tools/; python test.py --cfg ./cfgs/kitti_models/$architecture.yaml --eval_all --extra_tag $date)" C-m
 
 sleep 300
 
 # Create the second tmux session and run another command
 tmux new-session -d -s Session2
-tmux send-keys -t Session2 "(cd output/cfgs/kitti_models/$architecture/$date/eval/eval_all_default/default; tensorboard dev upload --logdir tensorboard_val --name \"${architecture//-/ } KITTI Evaluation $date\")" C-m
-
+tmux send-keys -t Session2 "(cd output/cfgs/kitti_models/$architecture/$date/eval/eval_all_default/default; tensorboard dev upload --logdir tensorboard_val --name "${architecture//-/ }_KITTI_Evaluation_$date")" C-m
