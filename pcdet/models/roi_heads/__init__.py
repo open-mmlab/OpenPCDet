e9c693ce3c88af7971662cee37745c43b60c2aea
Marc Brede <marc.brede@tum.de>
2023-09-09 13:57:32 +0000

    Tmp

--- pcdet/models/roi_heads/__init__.py
+++ pcdet/models/roi_heads/__init__.py
@@ -7,6 +7,7 @@ from .voxelrcnn_head import VoxelRCNNHead
 from .roi_head_template import RoIHeadTemplate
 from .mppnet_head import MPPNetHead
 from .mppnet_memory_bank_e2e import MPPNetHeadE2E
+from .roi_head import RoIHead
 
 __all__ = {
     'RoIHeadTemplate': RoIHeadTemplate,
@@ -18,4 +19,5 @@ __all__ = {
     'MPPNetHead': MPPNetHead,
     'MPPNetHeadE2E': MPPNetHeadE2E,
     'PVRCNNHeadRelation': PVRCNNHeadRelation,
+    'ROIHead': RoIHead,
 }
